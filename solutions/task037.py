ac0c3edabdc8ea79b598db67817f6ffd5364c88b
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-25 15:36:34 +1000

    037

--- solutions/task037.py
+++ solutions/task037.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=23:-n*g or[*zip(*eval(re.sub(r'(([^0]).{34})(?=(0.{34})+\2)',r'\1\2+',str(p(g,n-1)[::-1]))))]
+p=lambda g,n=39:-n*g or eval(re.sub(r'(([^0]).{34})(?=(0.{34})+\2)',r'\1\2+',str(p(g,n-1)[::-1])))
