83d5bf6c89d1a0670868dcc75138013c033d3225
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-08-25 23:56:22 +0200

    task201 - Probably slight potential with the cropping and removal of yellow bars

---
+++ solutions/task201.py
@@ -0,0 +1,2 @@
+F=filter
+def p(g):m=*F(any,[[(r[j],0)[4in{*r[:j+1]}&{*r[j:]}]for j in range(13)]for r in zip(*g)]),;l=sum(g,g);w=len(m);a,b,*_=l[l.index(4)+13::w+1];return[z:=[4]+w*[0]+[4],*[[a,*r[::1-2*(b in m[0])],b]for r in F(any,zip(*m))],z]
