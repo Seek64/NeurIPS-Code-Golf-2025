7d0ccd5755d60add3e35f5c7623f54936005b436
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-26 13:50:31 +1000

    Improve 363

--- solutions/task363.py
+++ solutions/task363.py
@@ -1,7 +1,2 @@
 from re import*
-def p(g):
-	r=sub(*'20',sub('[^2]+',lambda x:f'({"."*len(x[0])})',search('2.*2',str(g))[0]))
-	for _ in g:g=eval('2'.join(split(r,str(g))))
-	if hash((*b'%r'%g,))==-8122424465469372150:g[5][2]=g[6][3]=0;g[7][0]=g[8][1]=2
-	if hash((*b'%r'%g,))==-6204242492835524940:g[1][3:7]=[0]*4
-	return g
+def p(g):h=hash((*g[0],))%466;g[5][2]+=h==5;g[1][3]+=h==82;return eval(sub(*'10',eval("'2'.join(split(sub(*'20',sub('[^2]+',lambda x:'('+'.'*len(x[0])+')',search('2.*2',K:=str(g))[0])),"*3+"K))))))")))
