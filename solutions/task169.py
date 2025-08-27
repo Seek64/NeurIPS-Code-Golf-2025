14ad66246ddb512a0082b4c6682f718bb83e3b02
4atj <fourajbira16@gmail.com>
2025-08-27 08:38:11 +0100

    169 -2

--- solutions/task169.py
+++ solutions/task169.py
@@ -1,1 +1,1 @@
-p=lambda g,k=-999:[g:=[(a:=0)or[a:=c and F(k:=k+1,c,a,5-sum(g,g).count(c))for c in r]for*r,in zip(*g[::-1])]for F in[min]*15+[max]][-1]
+p=lambda g,k=11,o=999:-k*g or p([(a:=0)or[a:=c and[5-sum(g,g).count(c),max(o:=o-1,c,a)][k>0]for c in r][::-1]for*r,in zip(*g)],k-1,o)
