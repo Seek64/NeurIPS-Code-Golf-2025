4bbbd5d99b6b8d20ea6bba4f5a59a4b521d311b2
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-08-27 23:39:46 +0200

    335 -8b

--- solutions/task335.py
+++ solutions/task335.py
@@ -1,1 +1,4 @@
-E=enumerate;p=lambda g:[g:=[[r[j]or(any(r[:j])*any(r[j:])|any(c[:i])*any(c[i:])or(2in r)*(8in c))*4for j,c in E(zip(*g))]for i,r in E(g)]for _ in g][1]
+def p(g):
+ (i,j),(x,y)=map(divmod,map(sum(g,[]).index,[2,8]),2*[len(g[0])])
+ while j-y:x+=(x<i)-(i<x);g[x][y]=4;y+=(-1,1)[y<j]*(x==i)
+ return g
