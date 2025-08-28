47c6ba69af547f2447c255c9ed2301e9c3ad7f81
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-28 11:22:01 +1000

    314 -5

--- solutions/task314.py
+++ solutions/task314.py
@@ -1,2 +1,2 @@
-e=enumerate
-p=lambda g:[[max({*r[i::3]}&{*r[:i]}|{*c[j::3]}&{*c[:j+1]})for i,c in e(zip(*g))]for j,r in e(g)]
+import re
+p=lambda g:eval(re.sub(r'((?<=([^1]).{8})|(?<=(.).{77}))1(?=.{8}\2|.{77}\3)',r'\2\3',str(g)))
