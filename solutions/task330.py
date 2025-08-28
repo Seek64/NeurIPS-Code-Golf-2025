bd21ebccdde8db4063cf5108b634d405fe749292
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-08-28 23:15:26 +0200

    330 -23b

--- solutions/task330.py
+++ solutions/task330.py
@@ -1,1 +1,6 @@
-F=lambda l,q:sum([F(l,q+[k])for o in b"AKMW"if((k:=q[-1]+o-76)in q)*110<=k<110>0<l[k]],q);R=range(10);p=lambda g:[[g[i][j]%4*-~(6==len({*F(sum([r+[0]for r in g],[]),[11*i+j])}))for j in R]for i in R]
+def p(g):
+ for i in range(100):
+  q=[i+i//10]
+  for x in q:q+=[k for o in b"AKMW"if~-((k:=x+o-76)in q)*[*[*g,11*[0]][k//11],0][k%11]]
+  g[i//10][i%10]>>=2-(6==len(q))
+ return g
