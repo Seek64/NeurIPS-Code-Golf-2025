b5a6b3c3408cd753b438569305225592cc552415
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-28 16:23:22 +1000

    4atj trick

--- solutions/task143.py
+++ solutions/task143.py
@@ -1,4 +1,4 @@
 def p(g,i=1):
-	R=bytearray(G:=b'%r'%g);L=b'"%(BEH'
+	*R,=G=b'%r'%g;L=b'"%(BEH'
 	for k in L:R[k+i]=48|R[k]//49*5
-	return all(-G[k+i]|-G[k]<-47!={*G}!={*R}for k in L)and eval(R)or p(g,i+1)
+	return all(-G[k+i]|-G[k]<-47!={*G}!={*R}for k in L)and eval(bytes(R))or p(g,i+1)
