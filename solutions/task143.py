65a71f31f20dc782796b2a1eff5338bf9f806e45
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-28 13:39:43 +1000

    143 -1

--- solutions/task143.py
+++ solutions/task143.py
@@ -1,4 +1,4 @@
 def p(g,i=1):
 	*R,=G=sum(g,[]);L=b"\0\n"
 	for k in L:R[k+i]=R[k]and 5
-	return[R[i*10:][:10]for i in range(10)]*all(-G[k+i]^-G[k]>-1!={*G}!={*R}for k in L)or p(g,i+1)
+	return[[R.pop(0)for _ in r]for r in g*all(-G[k+i]^-G[k]>-1!={*G}!={*R}for k in L)]or p(g,i+1)
