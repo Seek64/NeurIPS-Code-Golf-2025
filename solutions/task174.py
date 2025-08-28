be55c2c9fa332b4c20f4587a1ab7cf25d7a26538
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-28 14:48:37 +1000

    174 -5

--- solutions/task174.py
+++ solutions/task174.py
@@ -1,3 +1,1 @@
-def p(g,i=1):
-	for _ in(w:=g):k=1;w=[r for*r,in zip(*w)if(k:=k&(r==r[::-1]))<=i in r]
-	return w*k or p(g,i+1)
+p=lambda g,i=1:[*zip(*(w:=eval((s:=f'[r for*r,in zip(*%s)if {i}in r]')%s%s%g)))]*(w==w[::-1])or p(g,i+1)
