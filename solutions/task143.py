4df53ca4611bbeb0467acabd1149e17a236b6980
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-28 16:11:16 +1000

    143 -2

--- solutions/task143.py
+++ solutions/task143.py
@@ -1,4 +1,4 @@
 def p(g,i=1):
-	*G,=R=bytearray(b'%r'%g);L=b'"%(BEH'
-	for k in L:R[k+i]=b"05"[R[k]>48]
+	R=bytearray(G:=b'%r'%g);L=b'"%(BEH'
+	for k in L:R[k+i]=48|R[k]//49*5
 	return all(-G[k+i]|-G[k]<-47!={*G}!={*R}for k in L)and eval(R)or p(g,i+1)
