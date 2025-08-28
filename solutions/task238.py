723db1ac6b3c417e8819b8ab8fbe58f6a1e7a14d
4atj <fourajbira16@gmail.com>
2025-08-28 13:04:27 +0100

    238 -14b ...

--- solutions/task238.py
+++ solutions/task238.py
@@ -1,6 +1,5 @@
-def p(g):
-	G=g
-	for _ in'@@':G=[r for*r,in zip(*G)if 8in r];g=[r for*r,in zip(*g)if{*r}-{8,0}]
+exec(f"""def p(g):
+	G=g;{"G=[r%sG)if 8in r];g=[r%sg)if{*r}-{8,0}];"*2}
 	for l in g[1:-1]:l[1:-1]=G.pop(0)
-	for _ in'@'*4:i=0;g=[(z:=min(i,len(g)-(i:=i+1)))%1or[c%~c&r[0]for c in r[:z]]+r[z:]for*r,in zip(*g[::-1])]
-	return g
+	{"i=0;g=[[c%%~c&r[0]for c in r[:z]]+r[z:]%sg[::-1])if[z:=min(i,len(g)-(i:=i+1))]];"*4}
+	return g"""%(*[" for*r,in zip(*"]*8,))
