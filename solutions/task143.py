0a697b72288bc7fe819b0eb905b64872a5e1d3f6
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-28 16:06:32 +1000

    bytearray wins again

--- solutions/task143.py
+++ solutions/task143.py
@@ -1,4 +1,4 @@
 def p(g,i=1):
-	*R,=G=sum(g,[]);L=b"\0\n"
-	for k in L:R[k+i]=R[k]and 5
-	return[[R.pop(0)for _ in r]for r in g*all(-G[k+i]^-G[k]>-1!={*G}!={*R}for k in L)]or p(g,i+1)
+	*G,=R=bytearray(b'%r'%g);L=b'"%(BEH'
+	for k in L:R[k+i]=b"05"[R[k]>48]
+	return all(-G[k+i]|-G[k]<-47!={*G}!={*R}for k in L)and eval(R)or p(g,i+1)
