5b2bc5d996b4b4304aa6fdc317965be570a83cf6
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-30 01:34:36 +1000

    yay

--- solutions/task023.py
+++ solutions/task023.py
@@ -1,6 +1,5 @@
 import re
 def p(g):
-	w=len(g[0])*3-2;l=[str(g)]*3
-	for s in['8?5(, )5(.{%d})5(, )5'%w,'2?5(, )5(, )5','2?5'+'(.{%d}...)5'%w*2]*4000:
-		G,*l=l;l+=[s[0].join(re.split(s,G,1))]*3
-		if('5'in G)<1:return eval(G)
+	w=len(g[0])*3-2;l=[f'{g}#28']*3
+	for s in['8?5(, )5(.{%d})5(, )5'%w,'2?5'+'(, )5'*2,'2?5'+'(.{%d}...)5'%w*2]*1300:G,*l=l;l+=[s[0].join(re.split(s,G,1))]*3
+	return eval(min(l,key=set))
