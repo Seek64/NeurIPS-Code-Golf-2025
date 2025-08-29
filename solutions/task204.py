f57904b603ef7269f57b54fb9fd7b30ec8335ad6
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-08-29 23:22:21 +0200

    204 -5b

--- solutions/task204.py
+++ solutions/task204.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:eval(re.sub(r'(0, |\[)(1[0, ]+)',lambda x:x[1]+x[2].replace('0','27'[len(x[0])%2]),str(g)))
+p=lambda g:eval(re.sub('([^1]..1)((, 0)+)',lambda x:x[1]+(k:=len(x[2])//3)*f',{2+k%2*5}',str(g)))
