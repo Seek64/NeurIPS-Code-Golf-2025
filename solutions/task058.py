1feb8915fcdbdc2655ff0c49be1f6ac131e1f460
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-30 00:55:53 +1000

    better 58

--- solutions/task058.py
+++ solutions/task058.py
@@ -1,4 +1,1 @@
-def p(g):
-	w=len(g);k=2-w%2;g=[[3]*k]*k
-	for _ in'@'*(~1&~-w):g=[[3,i,*r]for*r,i in zip(*g,[3]+[0]*w)][::-1]
-	return[*zip(*g)]
+def p(g):w,*L=len(g),3;k=2-w%2;g=[L*k]*k;exec('W=len(g);g[:]=L*W,[0]*~-W+L,*zip(*g[::-1]);'*(w-k));return g
