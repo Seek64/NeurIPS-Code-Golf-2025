7e7ff3ad2101b9f518b0c23645cb6d26dd0f9aa3
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-08-30 17:04:42 +0200

    task302 -13b

--- solutions/task302.py
+++ solutions/task302.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:eval(re.sub('([^5]..5)((, 0)+)',lambda x:x[1]+(k:=len(x[2])//3)*f',{5+k}',str(g)))
+p=lambda g:eval(re.sub('([^5]..5,)([, 0]+)',r'\1*(k:=len([\2]))*[k+5],',str(g)))
