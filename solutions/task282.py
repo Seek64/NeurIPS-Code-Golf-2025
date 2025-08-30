152a894c5cf885ca70aedb7a382cbc5a944d6d6f
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-08-30 18:42:46 +0200

    282 -14b

--- solutions/task282.py
+++ solutions/task282.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub(*s,str([*zip(*g[::-1])])))for s in["54",*6*[("0, 0(?=.{28}4)","5,1")],"40"]][7]
+p=lambda g:[g:=eval(re.sub('0, ([^0]), 0',r'2^\1,78%\1,2^\1',str([*zip(*g)])))for _ in g][1]
