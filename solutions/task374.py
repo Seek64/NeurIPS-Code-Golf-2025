b5d6cfa87408adb189bae386b626f3b50d96c8e8
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-08-31 16:29:50 +1000

    374 -2b

--- solutions/task374.py
+++ solutions/task374.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,i=43,c=1:-i*g or p(eval(s:=re.sub(i//4*", 5",i//4*f",{c}",S:=f"{[*zip(*g[::-1])]}")),i-1,(c+3*(s!=S))%5)
+p=lambda g,i=43,c=1:-i*g or p(eval(s:=re.sub(i//4*", 5",i//4*f",{c%5}",S:=f"{[*zip(*g[::-1])]}")),i-1,c+3*(s!=S))
