71488b9b9d649d8895da91ebac5929f1352ad158
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-08-31 17:20:58 +0200

    340 -19b

--- solutions/task340.py
+++ solutions/task340.py
@@ -1,1 +1,2 @@
-import re;p=lambda g:[g:=eval(re.sub(r"\((.), 0(?=[, 0-9]*\1)",r"(\1,\1",f"{[*zip(*g[::-1])]}"))for _ in 4*g]and g[:2]+[r[:2]+(len(g[0])-4)*(0,)+r[-2:]for r in g[2:-2]]+g[-2:]
+import re
+p=lambda g:[g:=eval(re.sub(*s,str([*zip(*g[::-1])])))for s in[(r"\((.), 0([^)]*)\1",r"(\1,\1\2 0")]*7+[("(\d)",r"\1*(sum(g,()).count(\1)>3)")]][7]
