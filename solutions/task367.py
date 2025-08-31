d8a7854c39d5cbe7657ecf84c3d0f12ac8b04177
4atj <fourajbira16@gmail.com>
2025-08-31 06:20:25 +0100

    367 -188b

--- solutions/task367.py
+++ solutions/task367.py
@@ -1,5 +1,1 @@
-e=enumerate
-def p(g):
- for i,r in e(g):
-  for j,x in e(r):exec("B=T=i;R=j+1\nwhile[]<r[R:]<[1]:R+=1\nwhile~-any([*g,g*9][B][j:R]):B+=1\nwhile~x&(c:=lambda a,b=j,c=R,h=g:1-(0<=a<len(h))or((H:=[1,1,*h[a],1,1])[b]<2>H[c+3])&all(H[b+1:c+2]))(i-1)&c(B)&c(j-1,i,B,h:=[*zip(*g)])&c(R,i,B,h)&(T<B):g[T][j:R]=[4]*(R-j);T+=1")
- return g
+p=lambda g,k=95:-k*g or[g:=[c*((r!=[5]*3)>c%-4<r[2])or(k+1in l)*4for*r,c in zip([4]*2+g*4,g*4,[4]+l,l)]for*l,in zip(*p(g,k-1)[::k%3-1|1])]
