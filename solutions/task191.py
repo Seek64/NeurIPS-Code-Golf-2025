4a7a721c38a520f16a6ac8b163657eb601a3fa2b
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-01 23:36:27 +1000

    Add 191

---
+++ solutions/task191.py
@@ -0,0 +1,5 @@
+from re import*
+def p(g):
+	L=[0]*25;g=L,*[[0,*r,0]for r in g],L;z=sub(*'10',sub('[^1]+',lambda x:f"({sub('[^%d]'%(4-(']'in x[0])),'.',x[0])})",search('1.*1',str(g))[0]))
+	for n in[1,-1,1]*4:*g,=zip(*eval('1'.join(split(z,str(g[::n])))))
+	return[r[1:-1]for r in g[1:-1]]
