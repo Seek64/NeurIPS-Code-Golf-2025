396e86e351584cee06f3244885107a764b873784
4atj <fourajbira16@gmail.com>
2025-09-01 08:36:05 +0100

    154/390 -10b

--- solutions/task154.py
+++ solutions/task154.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=0:[eval(re.sub("[^2]{9}2"*2+"?",lambda m:m[0][::-1],str(l)))for*l,in zip(*k or p(g,g))]
+p=lambda g,k=0:eval(re.sub("[50, ]{9}2"*2+"?","*[\g<0>][::-1]",str([*zip(*k or p(g,g))])))
--- solutions/task390.py
+++ solutions/task390.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,h=0:[eval(re.sub("[^2]{9}2"*2+"?",lambda s:s[0][::-1],str(r)))for r in zip(*h or p(g,g))]
+p=lambda g,k=0:eval(re.sub("[50, ]{9}2"*2+"?","*[\g<0>][::-1]",str([*zip(*k or p(g,g))])))
