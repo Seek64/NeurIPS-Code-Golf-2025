c2e4191451eb0ef463fbcc2fd840a9833bb7cb72
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-02 16:33:50 +1000

    392 -2b oops

--- solutions/task392.py
+++ solutions/task392.py
@@ -1,2 +1,2 @@
 R=range(10)
-p=lambda g:[[[w:=max(max(g)),5,5][min([max(abs(x-X),abs(y-Y))for X in R for Y in R if g[Y][X]])%(3-(f'0, {w}, '*2in'%s'%g))]for x in R]for y in R]
+p=lambda g:[[[w:=max(max(g)),5,5][min(max(abs(x-X),abs(y-Y))for X in R for Y in R if g[Y][X])%(3-(f'0, {w}, '*2in'%s'%g))]for x in R]for y in R]
