4a566f50b7de0f54d242b4974d402597fbadb354
4atj <fourajbira16@gmail.com>
2025-09-02 11:52:48 +0100

    240 -1b

--- solutions/task240.py
+++ solutions/task240.py
@@ -1,2 +1,2 @@
 R=range(19)
-p=lambda g,k=-7:k*g or p([[g[y][x]|g[y][[~x,y+2][18-y>x%2*x>y]]for y in R]for x in R],k+1)
+p=lambda g:[g:=[[g[y][x]|g[y][[~x,y+2][18-y>x%2*x>y]]for y in R]for x in R]for _ in R][5]
