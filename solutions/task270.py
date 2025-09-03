4b873ce83b374990d3d466b42ab4db3c1f19faee
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-03 12:10:26 +1000

    270 -13b

--- solutions/task270.py
+++ solutions/task270.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=-7:g*n or[eval(re.sub('(2), 0(.*)(, 3)|(1), 0(.*)(, 7)',r'\1\3\2\4\6\5,0',str(r)))for*r,in zip(*p(g,n+1)[::-1])]
+p=lambda g,n=7:g*-n or p(eval(re.sub(f'({1+n//4}),([^(]*)({7>>n//4})',r'\1,\3+\2 0',str([*zip(*g[::-1])]))),n-1)
