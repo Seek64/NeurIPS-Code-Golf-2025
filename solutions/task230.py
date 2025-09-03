18ec714fe77c89b5161752a3b83213da8cbeda5e
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-03 11:43:04 +1000

    230 -5b

--- solutions/task230.py
+++ solutions/task230.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub('0((.{%d}5){2})'%(len(g)*3+4),k+r'\1',str([*zip(*g[::-1])])))for k in'3421'*4][-1]
+p=lambda g:[g:=eval(re.sub('0(?=(.{%d}5){2})'%(len(g)*3+4),k,str([*zip(*g[::-1])])))for k in'3421'*2][7]
