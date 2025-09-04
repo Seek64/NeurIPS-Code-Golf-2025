77efc64931b58949f8e01447a01f5a2df786d8b5
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-04 16:49:52 +0200

    099 -1b

--- solutions/task099.py
+++ solutions/task099.py
@@ -1,1 +1,2 @@
-import re;p=lambda g:[g:=eval(re.sub("0(?=(%s.{28})([2-9]))"%s,r"\2",f"{[r[::-1]for r in g[::-1]]}"))for s in["..|..0"]*5+[".{60,66}|"]][-1]
+import re
+p=lambda g:[g:=eval(re.sub("0(?=(%s.{28})([2-9]))"%s,r"\2",f"{[r[::-1]for r in g[::-1]]}"))for s in["..|..0"]*5+[".{60,66}|"]][5]
