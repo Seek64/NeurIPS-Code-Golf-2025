5d94257006bceb9a560e32ef89f14a0e6b4c3cbe
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-09-04 22:58:35 +0200

    157 -56b

--- solutions/task157.py
+++ solutions/task157.py
@@ -1,14 +1,7 @@
-R=range(32)
-def p(g,*l):
- g=[*map(list,zip(*g))];b=[r.count(2)for r in g]+[3]
- for i in R:
-  z=i<16;i%=16;r=[*g,g][i]
-  if 5in r:l+=r[:],
-  elif n:=len(l):
-   for j in R:
-    j=-~j%(16-n);o=min(q:={y.index(5)-x for x,y in zip(b[j:],l)})
-    if(b[j+n]>2)*any(3>[*x,0].index(0)for x in g[j:j+n])*3-len(q)>z<n*(o>4-(n<2)or z)*(3in(b[j],b[j-1])):
-     for k in R[:n]:c=g[e:=j+k];d=b[e];g[e]=[a%4for a in c[:d]+l[k][d+o:]+4*[0]][:6]+c[6:];g[i+~k][6:]=[0]*4
-     break
-   l=[]
+R=range
+def p(g):
+ g=[*map(list,zip(*g))];m=[r.count(2)for r in g]
+ for i in R(64):
+  w=4-i//16;i%=16-w;l=g[i:i+w];*_,o,_,x=max((-(1in sum(g[j:j+w],l)),-len(q:={[*y,5].index(5)-x for x,y in zip(m[j:],l)}),min(q),m[j-1]+m[j+w-15],j)for j in R(16-w))
+  while w*all(5in r for r in l):w-=1;c=m[b:=x+w];g[b]=[z%4for z in g[b][:c]+l.pop()[c+o:]+4*[0]][:6]+g[b][6:];g[i+w][6:]=[0]*4
  return[*zip(*g)]
