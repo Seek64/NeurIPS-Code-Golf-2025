9fa750c56ac8268ab1477e578bcc285c0caf2078
4atj <fourajbira16@gmail.com>
2025-09-05 00:03:50 +0100

    44/175; more compressible

--- solutions/task044.py
+++ solutions/task044.py
@@ -1,3 +1,2 @@
 import re
-S=re.sub
-def p(g,x=9):b="5, 5, 5";u=b,*[str([5]+[(y!=x)*5for*c,y in zip(*g,r)if x in c]+[5])[1:-1]for r in g if x in r],b;v,y=re.subn(f"(.{{{32-len(u[1])}}})".join(u),S("0","x","\%d ".join(u))%(*range(1,len(u)),),str(g));return-x*g or p((y>0)*eval(S(str(x),"0",v))or g,x-1)
+def p(g,x=9):b="5, 5, 5";u=b,*[str([5]+[(y!=x)*5for*c,y in zip(*g,r)if x in c]+[5])[1:-1]for r in g if x in r],b;v,y=re.subn(f"(.{{{32-len(u[1])}}})".join(u),re.sub(*"0x","\%d ".join(u))%(*range(1,len(u)),),str(g));return-x*g or p((y>0)*eval(re.sub(str(x),"0",v))or g,x-1)
--- solutions/task157.py
+++ solutions/task157.py
@@ -1,7 +1,6 @@
-R=range
 def p(g):
  g=[*map(list,zip(*g))];m=[r.count(2)for r in g]
- for i in R(64):
-  w=4-i//16;i%=16-w;l=g[i:i+w];*_,o,_,x=max((-(1in sum(g[j:j+w],l)),-len(q:={[*y,5].index(5)-x for x,y in zip(m[j:],l)}),min(q),m[j-1]+m[j+w-15],j)for j in R(16-w))
-  while w*all(5in r for r in l):w-=1;c=m[b:=x+w];g[b]=[z%4for z in g[b][:c]+l.pop()[c+o:]+4*[0]][:6]+g[b][6:];g[i+w][6:]=[0]*4
+ for i in range(64):
+  w=4-i//16;i%=16-w;l=g[i:i+w];*_,o,_,x=max((-(1in sum(g[j:j+w],l)),-len(q:={[*y,5].index(5)-x for x,y in zip(m[j:],l)}),min(q),m[j-1]+m[j+w-15],j)for j in range(16-w))
+  while w*all(5in r for r in l):w-=1;c=m[b:=x+w];g[b]=[z%4for z in g[b][:c]+l.pop()[c+o:]+4*[0]][:6]+g[b][6:];g[i+w][6:]=4*[0]
  return[*zip(*g)]
