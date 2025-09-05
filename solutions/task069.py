f97eb3bc83db2e0888728d8b610b7d0491adc76d
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-05 11:45:53 +1000

    069 -13b

--- solutions/task069.py
+++ solutions/task069.py
@@ -1,6 +1,6 @@
-R=range(100)
+R=range(320)
 def p(g):
- f=sum(g,[]);P=[[i,f[i]]for i in R if f[i]%8]
+ *f,=U=b'%r'%g;P=[i for i in R if 88%f[i]%8]
  for i in R:
-  for j,x in(f[i]==8)*P:f[j],f[i+j-P[0][0]]=0,x
- return[f[i:i+10]for i in R[::10]]
+  for j in(f[i]==56)*P:f[j],f[i+j-P[0]]=48,U[j]
+ return eval(bytes(f))
