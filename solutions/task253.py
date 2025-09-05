951f7a50b7489e91f1862871aab8ce9d992fba13
4atj <fourajbira16@gmail.com>
2025-09-05 16:09:49 +0100

    253 -23b

--- solutions/task253.py
+++ solutions/task253.py
@@ -1,5 +1,2 @@
-import re
-def p(g):
- z=eval('[0]*4,'*4)
- for k in b"dsgltokpinvq":z[k%4][k%5]=int(re.search(r'([^0]), \1.{37}\1',str(g))[1]);*g,=zip(*g[::-1])
- return z
+F=0,1,2,3
+p=lambda g:[[max(min(g[p//12+v//2][p%12+v%2]for v in F if x*y%3|v^y&6^x//2^3)for p in range(144))for x in F]for y in F]
