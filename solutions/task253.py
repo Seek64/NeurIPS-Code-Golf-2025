f0669edc0cd0afeaf8f78f35af41472ab64ebbdf
lukegustafson <lukeg3@gmail.com>
2025-09-05 05:19:09 -0400

    253 fix (-1b)

--- solutions/task253.py
+++ solutions/task253.py
@@ -1,4 +1,5 @@
 import re
-def p(g,z=eval('[0]*4,'*4)):
-	for k in b"dsgltokpinvq":z[k%4][k%5]=int(re.search(r'([^0]), \1.{37}\1',str(g))[1]);*g,=zip(*g[::-1])
-	return z
+def p(g):
+ z=eval('[0]*4,'*4)
+ for k in b"dsgltokpinvq":z[k%4][k%5]=int(re.search(r'([^0]), \1.{37}\1',str(g))[1]);*g,=zip(*g[::-1])
+ return z
