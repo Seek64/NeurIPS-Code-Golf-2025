1352c4466fd494a3e76057d55e4d2d9efbab8df4
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-09-05 09:01:59 +0200

    157 -18b (better compression)

--- solutions/task157.py
+++ solutions/task157.py
@@ -1,6 +1,6 @@
 def p(g):
- g=[*map(list,zip(*g))];m=[r.count(2)for r in g]
+ g=[*map(list,zip(*g))]
  for i in range(64):
-  w=4-i//16;i%=16-w;l=g[i:i+w];*_,o,_,x=max((-(1in sum(g[j:j+w],l)),-len(q:={[*y,5].index(5)-x for x,y in zip(m[j:],l)}),min(q),m[j-1]+m[j+w-15],j)for j in range(16-w))
-  while w*all(5in r for r in l):w-=1;c=m[b:=x+w];g[b]=[z%4for z in g[b][:c]+l.pop()[c+o:]+4*[0]][:6]+g[b][6:];g[i+w][6:]=4*[0]
- return[*zip(*g)]
+  w=4-i//16;i%=16-w;*_,o,_,x=max((-(1in sum(g[j:j+w],l:=g[i:i+w])),-len(q:={[*y,5].index(5)-x.count(2)for x,y in zip(g[j:],l)}),min(q),(g[j-1]+g[j+w-15]).count(2),j)for j in range(16-w))
+  for j in range(w*all(5in r for r in l)):c=g[x+j].count(2);g[x+j]=[z%4for z in g[x+j][:c]+l[j][c+o:]+4*[0]][:6]+g[x+j][6:];g[i+j][6:]=4*[0]
+ return[*map(list,zip(*g))]
