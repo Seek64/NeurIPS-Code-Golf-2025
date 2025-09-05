2e51fe40b0ae8c9b26b1357c04a212fe33bb06dd
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-05 10:59:39 +1000

    037 -2b

--- solutions/task037.py
+++ solutions/task037.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=-9:n*g or eval(re.sub(r"(?<=([^0]).{34})(?=(.{35})*\1)0",r"\1",str(p(g,n+1)[::-1])))
+p=lambda g:eval(re.sub(r"(?<=([^0]).{34})(?=(.{35})*\1)0",r"\1",str(g[10230:]or p(g*2)[::-1])))
