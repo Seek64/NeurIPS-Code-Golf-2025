673214c5e0ab78948a58cea866d65b8ff4b366ca
4atj <fourajbira16@gmail.com>
2025-09-06 18:33:35 +0100

    245 -4b

--- solutions/task245.py
+++ solutions/task245.py
@@ -1,1 +1,1 @@
-p=lambda g,k=-15:k*g or p([[r[3>max(r)in[*filter(any,g)][0]]for r in zip(l,[0]+l)]for*l,in zip(*g)][::-1],k+1)
+p=lambda g,k=-15:k*g or p([[r[3>max(r)in max(g,key=any)]for r in zip(l,[0]+l)]for*l,in zip(*g)][::-1],k+1)
