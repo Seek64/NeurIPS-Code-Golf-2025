0c04bd31291c139e5e67488a0ccde8cc9e39a394
Mukundan314 <mukundan314@gmail.com>
2025-09-06 23:57:43 +0530

    182 -3b

--- solutions/task182.py
+++ solutions/task182.py
@@ -1,2 +1,2 @@
-import re
-p=lambda g,n=-3:n*g or p(eval(re.sub("(.{47})%s"*5%eval(re.sub("2|3","1",str(a:=re.findall(".{46}(.{15})5"*5+".{46}5",s:=str(g))[0]))),"\%d %%s"*5%(1,2,3,4,5)%a,s)),n+1)
+from re import*
+p=lambda g,n=-3:n*g or p(eval(sub("(.{47})%s"*5%eval(sub("2|3","1",str(a:=findall(".{46}(.{15})5"*5+".{46}5",s:=str(g))[0]))),"\%d %%s"*5%(1,2,3,4,5)%a,s)),n+1)
