6dec5082eb1959bb709503d12033de0968164152
4atj <fourajbira16@gmail.com>
2025-09-07 12:44:10 +0100

    131 -1b

--- solutions/task131.py
+++ solutions/task131.py
@@ -1,1 +1,1 @@
-p=lambda g,n=3:-n*g or p(sorted([*zip(*g[::-1]),len(g)*[8&max(g[0],key=bool)*6]],key=lambda r:g.extend(r)or any(r)/2or 2in g)[:-1],n-1)
+p=lambda g,n=3:-n*g or p(sorted([*zip(*g[::-1]),len(g)*[8&max(g[0],key=bool)*6]],key=lambda r:g.extend(r)or(2in g)*3^any(r))[:-1],n-1)
