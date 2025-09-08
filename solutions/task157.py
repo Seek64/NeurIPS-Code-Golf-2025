647e14cf59d875cdf188fd67d6922b6ea5d1c738
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-08 10:55:09 +0200

    157 -4b

--- solutions/task157.py
+++ solutions/task157.py
@@ -1,5 +1,5 @@
 def p(g):
  for i in range(64):
-  h=[*zip(*g)];w=4-i//16;i%=16-w;m=max((-(1in sum(h[j:j+w],())),-len(q:={y.index(max(y))-x.index(min(x))for x,y in zip(h[j:],h[i:i+w])}),min(q),j%w,j)for j in range(16-w))
-  for j in range(w*4*all(5in r for r in h[i:i+w])):g[6-m[2]+j%4][m[4]+j//4]+=h[i+j//4][6+j%4]%4;g[6+j%4][i+j//4]=0
+  *h,=zip(*g);w=4-i//16;i%=16-w
+  for j in range(w*4*all(5in r for r in h[i:i+w])):m=max((-(1in sum(h[j:j+w],())),-len(q:={y.index(5)-x.index(min(x))for x,y in zip(h[j:j+w],h[i:i+w])}),min(q),j%w,j)for j in range(16-w));g[6-m[2]+j%4][m[4]+j//4]+=h[i+j//4][6+j%4]%4;g[6+j%4][i+j//4]=0
  return g
