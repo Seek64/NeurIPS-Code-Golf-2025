4ea4ca64efdf941cf3acd083d5ce642ddd1ec9ba
4atj <fourajbira16@gmail.com>
2025-09-08 06:48:12 +0100

    more readable 182

--- solutions/task182.py
+++ solutions/task182.py
@@ -1,2 +1,2 @@
 from re import*
-p=lambda g:eval('eval(sub("(.{47})%s"*5%eval(sub("2|3","1",str(a:=findall(".{46}(.{15})5"*5+".{46}5",g:=str('*4+'g\n))[0]))),"\%d %%s"*5%(1,2,3,4,5)%a,g))#'*4)
+p=lambda g:[g:=eval(sub("(.{47})%s"*5%eval(sub("2|3","1",str(a:=findall(".{46}(.{15})5"*5+".{46}5",s:=str(g))[0]))),"\%d %%s"*5%(1,2,3,4,5)%a,s))for _ in g][3]
