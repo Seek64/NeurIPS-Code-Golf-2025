e2a33fe99ce085c27eccb5ff7c6b144abf385a27
Mukundan314 <mukundan314@gmail.com>
2025-09-08 09:52:50 +0530

    361 -15b

--- solutions/task361.py
+++ solutions/task361.py
@@ -1,3 +1,2 @@
 import re
-Z=9*[[0]*10]
-def p(g):s,e=re.search(r"( [^0]{7}.{24}){3}|(([^0]), \3).{28}\2.{28}",str(g)).span();x=s%32//3;return[g:=[[*map(max,*r)]for r in zip(g,[*Z,*zip(*(Z+g[::-1]+Z)[19-x-e//32:])][9-s//32+x:]+Z)]for _ in g][3]
+def p(g):s,e=re.search(r"( [^0]{7}.{24}){3}|(([^0]), \3).{28}\2.{28}",str(g)).span();x=s%32//3;return[g:=[[*map(max,*r)]for r in zip(g,(3*[*zip(*(3*g[::-1])[20-x-e//32:])])[10-s//32+x:])]for _ in g][3]
