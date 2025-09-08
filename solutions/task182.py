39164426e302f006241ccb1d76419bbf017827e7
4atj <fourajbira16@gmail.com>
2025-09-08 06:21:10 +0100

    182 1b

--- solutions/task182.py
+++ solutions/task182.py
@@ -1,2 +1,2 @@
 from re import*
-p=lambda g,n=-3:n*g or p(eval(sub("(.{47})%s"*5%eval(sub("2|3","1",str(a:=findall(".{46}(.{15})5"*5+".{46}5",s:=str(g))[0]))),"\%d %%s"*5%(1,2,3,4,5)%a,s)),n+1)
+p=lambda g:eval('eval(sub("(.{47})%s"*5%eval(sub("2|3","1",str(a:=findall(".{46}(.{15})5"*5+".{46}5",g:=str('*4+'g\n))[0]))),"\%d %%s"*5%(1,2,3,4,5)%a,g))#'*4)
