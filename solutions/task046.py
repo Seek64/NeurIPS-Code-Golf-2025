615e5ab3d0c9c41893721829d504326c174ae6d9
4atj <fourajbira16@gmail.com>
2025-09-08 21:34:52 +0100

    046 -8b

--- solutions/task046.py
+++ solutions/task046.py
@@ -1,2 +1,1 @@
-*Z,h=(0,)*3,lambda g,d=3:[[x and sum({*q+r+s}-{5})for x in 3*r][d:3+d]for q,r,s in zip(Z+g,g,g[1:]+Z)if Z<[r]or(d:=d-[*q,5].index(5)+[*s,5].index(5))*0]
-p=lambda g:[*zip(*h([*zip(*g)]))]
+*Z,p=(0,)*3,lambda g,d=3:[g:=[*zip(*g)]]and[*zip(*[[x%~x&sum({*q+r+s}-{5})for x in 3*r][d:3+d]for q,r,s in zip(Z+g,g,g[1:]+Z)if Z<[r]or(d:=d-[*q,5].index(5)+[*s,5].index(5))*0])]
