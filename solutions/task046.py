ce3562b84a9f06c54b3e0447da09e02c2387954d
Mukundan314 <mukundan314@gmail.com>
2025-09-08 09:37:56 +0530

    046 -5b

--- solutions/task046.py
+++ solutions/task046.py
@@ -1,2 +1,2 @@
-*Z,h=(0,)*3,lambda g,d=0:[[x and sum({*q+r+s}-{5})for x in 3*r][3-d:][:3]for q,r,s in zip(Z+g,g,g[1:]+Z)if any(r)or(5in q and(d:=d+q.index(5)-s.index(5)))*0]
+*Z,h=(0,)*3,lambda g,d=3:[[x and sum({*q+r+s}-{5})for x in 3*r][d:3+d]for q,r,s in zip(Z+g,g,g[1:]+Z)if Z<[r]or(d:=d-[*q,5].index(5)+[*s,5].index(5))*0]
 p=lambda g:[*zip(*h([*zip(*g)]))]
