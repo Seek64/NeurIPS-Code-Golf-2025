5e540dc06702423d896a576f18d471c7f6717891
lukegustafson <lukeg3@gmail.com>
2025-09-08 20:08:01 -0400

    merge in garrymoss (3 new, -170b existing)

--- solutions/task158.py
+++ solutions/task158.py
@@ -1,10 +1,1 @@
-def p(g):
- e=lambda i,j:[(a,b)for a in range(i)for b in range(j)]
- T=lambda i,j,m:[r[j:j+m]for r in g[i:i+m]];h=len(g);w=len(g[0]);[(G:=H)for i,j in e(h-2,w-2)if len({*sum(H:=T(i,j,3),[])})>3];X=0
- if G[0][0]in sum(G,[])[1:]:G=G[::-1]
- while X<12:
-  X+=1;m=X%3+1;g=[*map(list,zip(*g))][::-1];h,w=w,h
-  for i,j in e(h-3*m+1,w-3*m+1):
-    if all(T(i+k*m,j+k*m,m)==[[G[k][k]]*m]*m for k in(0,2))*(g[i-1][j]^g[i][j]):
-     for x,y in e(m*3,m*3):g[i+x][j+y]=G[x//m][y//m]
- return g
+def p(n):o=max(n[0],key=n[0].count);f,=[r for a in(n,n[::-1])for g in range(len(n)-2)for h in range(len(n[0])-2)if len(set(r:=[a[g+p//3][h+p%3]for p in range(9)]))>3>0<o!=r[0]!=r[8]!=o];[(t:=lambda d=o,b=o,c=o,*z:[d]*u+[b]*u+[c]*u)(t(f[0]),t(),t(c=f[8]))!=[a[g+p][h:[h+3*u*s,None][h+3*u*s<0]:s]for p in range(3*u)]or exec('for p in range(3*u):a[g+p][h:[h+3*u*s,None][h+3*u*s<0]:s]=t(*f[p//u*3:])')for u in range(4)for s in(1,-1)for g in range(len(n)-3*u+1)for h in range(len(n[0]))for a in(n,n[::-1])];return n
