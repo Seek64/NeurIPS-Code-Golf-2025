b4a30e89ea7e0124dd531ea426c258e83ab651a3
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-09-09 12:43:24 +1000

    396 -13b

--- solutions/task396.py
+++ solutions/task396.py
@@ -1,5 +1,2 @@
 import re
-def p(g):
-	a,b,_=sorted(set(w:=sum(g,[])),key=w.count)
-	for _ in g*4:*g,=zip(*g[(max(re.findall(f'{b}(?:, {b})+','%s'%g))in str(g[-1]))-2::-1])
-	return[[a*(c>0)for c in r]for r in g]
+def p(g,S=str):*_,a,b=sorted('123456789',key=S(g).count);exec("g[:]=zip(*g[(max(re.findall(b+f'(?:, {b})+',S(g)))in S(g[-1]))-2::-1]);"*96);return eval(re.sub(b,a,S(g)))
