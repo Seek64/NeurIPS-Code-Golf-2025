cfc88fa77802d08aee0e3d989527943396267e1b
Mukundan314 <mukundan314@gmail.com>
2025-09-10 18:13:04 +0530

    110 -9b

--- solutions/task110.py
+++ solutions/task110.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub("0(?=([^0)]{15}).*(.)\\1)","\\2",str([*zip(*g)][::-1])))for _ in g][27]
+p=lambda g:eval([g:=re.sub(r"0(?=([^]0]{15}).*(.)\1)",r"\2",str(g)[::-1])for _ in g][-2])
