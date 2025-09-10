0a1eeb62851082f2c4ed1f7e41a111f091689091
Mukundan314 <mukundan314@gmail.com>
2025-09-10 15:15:58 +0530

    206 -10b

--- solutions/task206.py
+++ solutions/task206.py
@@ -1,4 +1,4 @@
 def p(g):
  t=sum(g,[]).index(5);l=len(g[0])
- for r,a in zip(g[t//l-1:][:3],[[x for*c,x in zip(*g,r)if{*c}-{0,5}]for r in g if{*r}-{0,5}]):r[t%l-1:t%l+2]=a
+ for a in[[x for*c,x in zip(*g,r)if{*c}-{0,5}]for r in g if{*r}-{0,5}]:g[t//l-1][t%l-1:t%l+2]=a;t+=l
  return g
