eac7438a3dfa70aa8d80b7675d2411d4e739d823
Mukundan314 <mukundan314@gmail.com>
2025-09-10 21:29:21 +0530

    143 -20b

--- solutions/task143.py
+++ solutions/task143.py
@@ -1,4 +1,4 @@
 def p(g,i=1):
-	*R,=G=b'%r'%g;L=b'"%(BEH'
-	for k in L:R[k+i]=48|R[k]//49*5
-	return all(-G[k+i]|-G[k]<-47!={*G}!={*R}for k in L)and eval(bytes(R))or p(g,i+1)
+	*R,=G=b'%r'%g
+	for k in b'"%(BEH':R[k+i]=-R[k+i]|-R[k]>-48or 48|R[k]//49*5
+	return{*G}>{*R}and eval(bytes(R))or p(g,i+1)
