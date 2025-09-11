bdbc83c22b4de7e7b074c3d783ca14cb1eb1180d
4atj <fourajbira16@gmail.com>
2025-09-11 07:50:09 +0100

    191 -1b (compressed)

--- solutions/task191.py
+++ solutions/task191.py
@@ -1,5 +1,5 @@
-from re import*
+import re
 def p(g):
-	L=[0]*25;g=L,*[[0,*r,0]for r in g],L;z=sub(*'10',sub('[^1]+',lambda x:f"({sub('[^%d]'%(4-(']'in x[0])),'.',x[0])})",search('1.*1',str(g))[0]))
-	for n in[1,-1,1]*4:*g,=zip(*eval('1'.join(split(z,str(g[::n])))))
+	L=[0]*25;g=L,*[[0,*r,0]for r in g],L;z=re.sub(*'10',re.sub('[^1]+',lambda x:f"({re.sub('[^%d]'%(4-(']'in x[0])),'.',x[0])})",re.search('1.*1',str(g))[0]))
+	for n in[1,-1,1]*4:*g,=zip(*eval('1'.join(re.split(z,str(g[::n])))))
 	return[r[1:-1]for r in g[1:-1]]
