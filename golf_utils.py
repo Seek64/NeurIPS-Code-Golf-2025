488247bf07687c69d2d499f86a244ce32dfd2776
Mukundan314 <mukundan314@gmail.com>
2025-09-11 11:15:36 +0530

    better compression -14b

--- golf_utils.py
+++ golf_utils.py
@@ -4,62 +4,80 @@ import deflate
 
 from reencoder import reencode
 
+ZLIB_LEVELS = [9]
+ZOPFLI_ITERS = [256]
+DEFLATE_LEVELS = range(7, 13)
+DELIMS = [b"'", b'"']
 
-def pack(src: bytes) -> bytes:
-    """Given a python program as a bytes object, returns a possibly shorter python program"""
+# ZLIB_LEVELS = range(-1, 10)
+# ZOPFLI_ITERS = [1 << i for i in range(10)]
+# DEFLATE_LEVELS = range(1, 13)
+# DELIMS = [b"'", b'"', b"'''"]
 
-    def sanitize(b_in: bytes, delim: bytes) -> bytes:
-        """Clean up problematic bytes in compressed b-string"""
-        b_in = reencode(b_in, delim)
-        b_out = bytearray()
-        for b, b_next in zip(b_in, [*b_in[1:], 0]):
-            if b == 0:
-                if b_next in b"01234567":
-                    b_out += b"\\x00"
-                else:
-                    b_out += b"\\0"
-            elif b == ord("\r"):
-                b_out += b"\\r"
-            elif b == ord("\\") and b_next in b"01234567abfxnrtvuUN'\"\\":
-                b_out += b"\\\\"
-            elif b == ord("\n") and len(delim) == 1:
-                b_out += b"\\n"
-            elif bytes([b]) == delim:
-                b_out += b"\\" + delim
-            else:
-                b_out.append(b)
-        return bytes(b_out)
 
-    codes = [src]
+def sanitize(b_in: bytes, delim: bytes) -> bytes:
+    """Clean up problematic bytes in compressed b-string"""
+    b_in = reencode(b_in, delim)
+    b_out = bytearray()
+    for b, b_next in zip(b_in, [*b_in[1:], 0]):
+        if b == 0:
+            b_out += b"\\x00" if b_next in b"01234567" else b"\\0"
+        elif b == ord("\r"):
+            b_out += b"\\r"
+        elif b == ord("\\") and b_next in b"01234567abfxnrtvuUN'\"\\":
+            b_out += b"\\\\"
+        elif b == ord("\n") and len(delim) == 1:
+            b_out += b"\\n"
+        elif bytes([b]) == delim:
+            b_out += b"\\" + delim
+        else:
+            b_out.append(b)
+    return bytes(b_out)
 
+
+def compress(src: bytes) -> bytes:
+    """Given a bytes object, returns a zlib compressed literal"""
     compressed: list[bytes | bytearray] = []
 
     # Standard zlib attempts
-    # for level in range(-1, 10):
-    #     compressed.append(zlib.compress(src, level=level))
-    compressed.append(zlib.compress(src, level=9))
-
+    for level in ZLIB_LEVELS:
+        # compressed.append(zlib.compress(src, level=level)[2:-4])
+        compressor = zlib.compressobj(wbits=-10)
+        compressor.compress(src)
+        compressed.append(compressor.flush())
     # Zopfli attempts
-    # for i in range(10):
-    #     compressed.append(zopfli.zlib.compress(src, numiterations=1 << i)[2:-4]) # type: ignore[reportUnknownArgumentType]
-    compressed.append(zopfli.zlib.compress(src, numiterations=256)[2:-4]) # type: ignore[reportUnknownArgumentType]
-
+    for i in ZOPFLI_ITERS:
+        compressed.append(zopfli.zlib.compress(src, numiterations=i)[2:-4])  # type: ignore[reportUnknownArgumentType]
     # deflate (libdeflate) attempts
-    # for level in range(1, 13):
-    for level in range(7, 13):
-        compressed.append(deflate.deflate_compress(src, compresslevel=level)) # type: ignore[reportUnknownArgumentType]
+    for level in DEFLATE_LEVELS:
+        compressed.append(deflate.deflate_compress(src, compresslevel=level))  # type: ignore[reportUnknownArgumentType]
 
-    # Wrap compressed bytes in Python exec statements
-    for compressed_code in compressed:
-        # for delim in [b"'", b'"', b"'''"]:
-        for delim in [b"'", b'"']:
-            codes.append(
-                b"#coding:L1\nimport zlib\nexec(zlib.decompress(bytes("
-                + delim
-                + sanitize(compressed_code, delim)
-                + delim
-                + b',"L1"),~9))'
-            )
+    literals: list[bytes] = []
+    for data in compressed:
+        for delim in DELIMS:
+            literals.append(delim + sanitize(data, delim) + delim)
 
-    # Return the shortest
+    return min(literals, key=len)
+
+
+def pack(src: bytes) -> bytes:
+    """Given a python program as a bytes object, returns a possibly shorter python program"""
+    codes = [src]
+
+    codes.append(
+        b"#coding:L1\nimport zlib\nexec(zlib.decompress(bytes("
+        + compress(src)
+        + b',"L1"),~9))'
+    )
+
+    if src.startswith(b"import"):
+        imports = src.split()[1]
+        codes.append(
+            b"#coding:L1\nimport zlib,"
+            + imports
+            + b"\nexec(zlib.decompress(bytes("
+            + compress(src[len(imports) + 8:])
+            + b',"L1"),~9))'
+        )
+
     return min(codes, key=len)
