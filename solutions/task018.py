c4eb1bfef139ebbfd397ba6ccb46c83a4116bbe7
Mukundan314 <mukundan314@gmail.com>
2025-09-13 00:23:23 +0530

    018 -42 (post compression)

--- solutions/task018.py
+++ solutions/task018.py
@@ -1,14 +1,13 @@
-def T(V):_,x,y=V[0];return[*zip(*[((u,x+y-c,r-x+y),(u,c-y+x,x+y-r),(u,2*x-r,c),(u,r,2*y-c))for u,r,c in V])]
+def T(O):return*zip(*[((u,c,r),(u,-r,c),(u,r,-c))for u,r,c in O]),
 def p(d,*E):
  R,H,W,*U=range,len(d),len(d[0])
  for z in R(H*W):
   O=[(v:=d[i:=z//W][j:=z%W],i,j)]*v
   for u,r,c in O:d[r][c]=0;O+=[(u,x,y)for a in R(9)if W>(y:=c+a%3-1)>=0<H>(x:=r+a//3-1)>=0<(u:=d[x][y])]
-  if len({*O})>3:E+=O,
+  if len({*O})>3:E+=T((u,r-i,c-j)for u,r,c in O);E+=T(E[-3])
   else:U+=O
- for O in E:
-  for i in R(-H,H):
-   for j in R(-W,W):
-    B=T(Q:=[(u,r+i,c+j)for u,r,c in{*O}])
-    for u,r,c in[*[I for I in[Q]+B+T(B[0])if len({*U}&{*I})>2],[]][0]:d[r][c]=u
+ for*O,in E:
+  for z in R(H*W):
+   S=[(u,r+z//W,c+z%W)for u,r,c in O]
+   for u,r,c in(len({*U}&{*S})>2)*S:d[r][c]=u
  return d
