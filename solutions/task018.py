174090a5cf7ea9b7dc0135b5e190027e5a90c918
Mukundan314 <mukundan314@gmail.com>
2025-09-13 21:07:38 +0530

    018 -13b (post compression)

--- solutions/task018.py
+++ solutions/task018.py
@@ -1,13 +1,10 @@
-def T(O):return*zip(*[((u,c,r),(u,-r,c),(u,r,-c))for u,r,c in O]),
+def T(O):return*zip(*[((u,c,r),(u,-r,c))for u,r,c in O]),
 def p(d,*E):
  R,H,W,*U=range,len(d),len(d[0])
  for z in R(H*W):
-  O=[(v:=d[i:=z//W][j:=z%W],i,j)]*v
-  for u,r,c in O:d[r][c]=0;O+=[(u,x,y)for a in R(9)if W>(y:=c+a%3-1)>=0<H>(x:=r+a//3-1)>=0<(u:=d[x][y])]
-  if len({*O})>3:E+=T((u,r-i,c-j)for u,r,c in O);E+=T(E[-3])
-  else:U+=O
- for*O,in E:
+  for u,r,c in(O:=[(v:=d[i:=z//W][j:=z%W],i,j)]*v):d[r][c]=0;O+=[(u,x,y)for z in R(9)if(x:=r+z//3-1)//H|(y:=c+z%3-1)//W==0<(u:=d[x][y])]
+  U+=O*(len(O)<5*v);E+=T((u,r-i,c-j)for u,r,c in O)
+ for*O,in sum(map(T,E),E):
   for z in R(H*W):
-   S=[(u,r+z//W,c+z%W)for u,r,c in O]
-   for u,r,c in(len({*U}&{*S})>2)*S:d[r][c]=u
+   for u,r,c in(S:=[(u,r+z//W,c+z%W)for u,r,c in O])*(len({*U}&{*S})>2):d[r][c]=u
  return d
