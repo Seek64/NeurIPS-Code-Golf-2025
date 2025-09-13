53400f12efe62d9675a5504d8b403778b59cf3ec
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-13 14:42:09 +0200

    054 -11b

--- solutions/task054.py
+++ solutions/task054.py
@@ -1,6 +1,6 @@
 import re
 def p(g):
- l=sum(g,[]);a,b,c,*d=sorted({*l},key=lambda x:l[::-1].index(x)+l.index(x)-l.count(x))
- for i in[i-62for i in range(900)if(l[i]==c)and({*l[i-1:i+2]}&{a,b}or(j:=i-62))]+[j]:
-  for o in range(25):g[i//30+o//5][i%30+o%5]=a*(i==j)or(l[i+o//5*30+o%5],l[j+o//5*30+o%5])[l[j+o//5*30+o%5]!=a!=b==l[i+o//5*30+o%5]]
+ l=sum(g,[]);a,b,c,*d={}.fromkeys((14*l[1:])[7::30]+l[240:])
+ for i in[i for i in range(838)if(l[i+62]==c)and({*l[i+61:i+64]}&{a,b}or(j:=i))]+[j]:
+  for o in range(25):g[i//30+o//5][i%30+o%5]=a*(i==j)or(l[o//5*30+o%5+i],l[o//5*30+o%5+j])[l[o//5*30+o%5+j]!=a!=b==l[o//5*30+o%5+i]]
  return[g:=eval(re.sub(f"{b}, (?=([^{a}], )\\1+{c})","\\1",str([*zip(*g[::-1])])))for i in 2*g][-1]
