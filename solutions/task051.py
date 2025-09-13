0f12c9ff5397b3d68996edcc48a663311af926f8
Mukundan314 <mukundan314@gmail.com>
2025-09-13 17:53:21 +0530

    051 -1b

--- solutions/task051.py
+++ solutions/task051.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=3:-n*g or p(eval(re.sub(r"0(?=[^)]*([^0]), ((?!\1|0).), 0)",r"\2",str([*zip(*g[::-1])]))),n-1)
+p=lambda g:[g:=eval(re.sub(r"0(?=[^)]*([^0]), ((?!\1|0).), 0)",r"\2",str([*zip(*g[::-1])])))for _ in g][3]
