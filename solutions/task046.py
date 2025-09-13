29cc9c86e01a08fd3ea9c294ce0f5a99c9105dba
Mukundan314 <mukundan314@gmail.com>
2025-09-13 16:02:20 +0530

    046 -4b

--- solutions/task046.py
+++ solutions/task046.py
@@ -1,1 +1,1 @@
-*Z,p=(0,)*3,lambda g,d=3:[g:=[*zip(*g)]]and[*zip(*[[x%~x&sum({*q+r+s}-{5})for x in 3*r][d:3+d]for q,r,s in zip(Z+g,g,g[1:]+Z)if Z<[r]or(d:=d-[*q,5].index(5)+[*s,5].index(5))*0])]
+*Z,p=(5,)*3,lambda g,d=3:[g:=[*zip(*g)]]and[*zip(*[[x%~x&sum({*q+r+s}-{5})for x in 3*r][d:3+d]for q,r,s in zip(Z+g,g,g[1:]+Z)if any(r)or(d:=d-[*q,5].index(5)+s.index(5))*0])]
