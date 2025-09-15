1ab651adc69ea18a3f73c7dbc62f01c6fc0780dc
4atj <fourajbira16@gmail.com>
2025-09-15 20:06:25 +0100

    342 -1b (by fine-tuned qwen2.5 14b)

--- solutions/task342.py
+++ solutions/task342.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=-3:n*g or p(eval(re.sub("([1-9])((.{32})+?[^)]+?)8",r"0\2\1",str([*zip(*g[::-1])]))),n+1)
+p=lambda g:[g:=eval(re.sub("([1-9])((.{32})+?[^)]+?)8",r"0\2\1",str([*zip(*g[::-1])])))for _ in g][3]
