abf3cce52466b39947dc0e24326ba42e10047324
lukegustafson <lukeg3@gmail.com>
2025-09-15 12:26:40 -0400

    090 -9b

--- solutions/task090.py
+++ solutions/task090.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:eval('6'.join(max([re.split((f'(.{{{len(g[0])*3-i%7*3-2}}}){("(, )0"*(i%7+2))[4:]}'*(i%5+2))[7:],str(g),1)for i in range(35)],key=len)))
+p=lambda g:eval('6'.join(max([re.split((f'(.{{{len(g[0])*3-i%8*3+1}}})0{i%8*"(, )0"}'*(i%5+2))[7:],str(g),1)for i in range(40)],key=len)))
