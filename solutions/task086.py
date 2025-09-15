76a70f52b55df9f7e3763b3bd1b205f6f4828eaf
Mukundan314 <mukundan314@gmail.com>
2025-09-15 14:11:56 +0530

    086 -1b

--- solutions/task086.py
+++ solutions/task086.py
@@ -1,3 +1,2 @@
 E=enumerate
-o=lambda r,i:(s:=r*2)[i+1]|s[i-1]or(len(r)-r.count(0))%3*(s[i+2]|s[i-2])
-p=lambda g:[[sum({*sum(g*x,[-x])})or o(c,i)|o(r,j)for j,(*c,x)in E(zip(*g,r))]for i,r in E(g)]
+p=lambda g,i=-1:i<0and[[sum({*sum(g*x,[-x])})or p(c,i)|p(r,j)for j,(*c,x)in E(zip(*g,r))]for i,r in E(g)]or(s:=g*2)[i+1]|s[i-1]or(len(g)-g.count(0))%3*(s[i+2]|s[i-2])
