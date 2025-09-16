d671308d3aca581d27755135229dbc6a7b6b9b31
Mukundan314 <mukundan314@gmail.com>
2025-09-16 13:02:10 +0530

    120 -1b

--- solutions/task120.py
+++ solutions/task120.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:eval(re.sub(r"([^0])(?<=\1.{%d}\d)(?=.{%d}\1)"%(x:=len(g[0])*3+4,x),"8",str(g)))
+p=lambda g:eval(re.sub(r"(?<=([1-9]).{%d})\1(?=.{%d}\1)"%(x:=len(g[0])*3+4,x),"8",str(g)))
