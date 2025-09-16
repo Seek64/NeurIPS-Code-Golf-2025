4dbd229b89fa299c438e5862a5d180647dc9642e
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-16 13:20:27 +0200

    324 -7b

--- solutions/task324.py
+++ solutions/task324.py
@@ -1,9 +1,9 @@
 def p(g,*t):
- l=sum(g,[]);m={};*f,j,y=sorted({*l},key=l.count)
+ l=sum(g,[]);*f,j,y=sorted({*l},key=l.count)
  for i in range(len(g)):
   for j in range(len(g[i])):
-   if g[i][j]in f:m[g[i][j]]=g[i][j];m[max(l:=[g[i][j-1],g[i][j-len(g[i])+1],g[i-1][j],g[i-len(g)+1][j]],key=lambda x:(l.count(x),-(len(g[i])*[x]in g)))]=g[i][j];f+=-i-j,;t+=i-j,
+   if g[i][j]in f:l[g[i][j]]=g[i][j];l[max(l,key=lambda x:([g[i][j-1],g[i][j-len(g[i])+1],g[i-1][j],g[i-len(g)+1][j]].count(x),-(len(g[i])*[x]in g)))]=g[i][j];f+=-i-j,;t+=i-j,
  for i in range(len(g)):
   for j in range(len(g[i])):
-   if i-j in t or-i-j in f:g[i][j]=m[g[i][j]]
+   if i-j in t or-i-j in f:g[i][j]=l[g[i][j]]
  return g
