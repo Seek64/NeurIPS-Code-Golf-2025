ce5c538a5b4a9f6d6e8a624d9ed45c8e4294db98
Mukundan314 <mukundan314@gmail.com>
2025-09-17 17:48:53 +0530

    153 -7b

--- solutions/task153.py
+++ solutions/task153.py
@@ -1,1 +1,1 @@
-exec(f"T=0,1,2;p=lambda g:[G {'for %s in range(~9,0)'*4%(*'ijkl',)}if(G:=[[g[x+i][y+j]^g[x+k][y+l]for y in T]for x in T])*all(sum(G,G))][0]")
+exec(f"T=0,1,2;p=lambda g:max(all(sum(G:=[[g[x-i][y-j]^g[x-k][y-l]for y in T]for x in T],G))*G {'for %s in range(10)'*4%(*'ijkl',)})")
