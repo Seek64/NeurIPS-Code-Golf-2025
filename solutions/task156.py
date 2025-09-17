c7ea34c4bb1a6829eabdf502b30293064f14b7fa
Mukundan314 <mukundan314@gmail.com>
2025-09-17 18:10:53 +0530

    156 -6b

--- solutions/task156.py
+++ solutions/task156.py
@@ -1,1 +1,2 @@
-import re;p=lambda g:eval("".join(re.sub("(?<=4.{28}4, )4(?=, 4.{28}4)","12"[G.count("4")*8>sum(sum(g,[]))],G)for G in re.split(r"(\[[^4]*\])",str(g))))
+import re
+p=lambda g:eval("".join(re.sub("(?<=4.{34})4(?=.{34}4)","12"[G.count("4")*8>sum(sum(g,[]))],G)for G in re.split(r"(\[[^4]*\])",str(g))))
