8f27e12f8009c7897026f02c33a219ac53c3ff61
4atj <fourajbira16@gmail.com>
2025-09-18 23:57:25 +0100

    275 -1b (by fine-tuned Qwen2.5-Coder-3B-Instruct)

--- solutions/task275.py
+++ solutions/task275.py
@@ -1,4 +1,4 @@
 def p(g):
  l=len(g);a,b=g[:l//2],g[l//2:]
- if'8'in str(a):a,b=b,a
+ if'8'in'%s'%a:a,b=b,a
  return[[l//5*k for k in i for l in j]for i in a for j in b]*(l>5)or[*zip(*p([*zip(*g)]))]
