adadf8383df44f2304e0b3b508b2be12410320ee
lukegustafson <lukeg3@gmail.com>
2025-09-18 02:36:20 -0400

    096 -4b

--- solutions/task096.py
+++ solutions/task096.py
@@ -2,7 +2,7 @@ import re
 def p(g):
  m=-1;a=[max(S:=sum(g,[]),key=S.count)]
  for l in range(6,0,-1):
-  if M:=[(i,c)for i in range(1%l+1,7)for c in{*S}-{*a}if re.search(f"{c}.."*i+f"[^{c}].."*(2*l-1-2*i)+f"{c}"*(i<l),f"{g,*zip(*g)}"+f"{g,*zip(*g)}"[::-1])]:
+  if M:=[i*[c]for i in range(1%l+1,7)for c in{*S}-{*a}if re.search(f"{c}.."*i+f"[^{c}].."*(2*l-1-2*i)+f"{c}"*(i<l),f"{g,*zip(*g)}"+f"{g,*zip(*g)}"[::-1])]:
    if(m:=m+1)<1:r=[a*(2*l-1)]*(2*l-1)
-   for i in(1,1,-1)*4:r=[r[::i]for*r,in zip(*r)];r[m][m:m+M[0][0]]=[M[0][1]]*M[0][0];a+=M[0][1],
+   for i in(1,1,-1)*4:r=[r[::i]for*r,in zip(*r)];a+=M[0];r[m][m:m+len(M[0])]=M[0]
  return r
