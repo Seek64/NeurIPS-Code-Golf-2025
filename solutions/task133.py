33f72e5498585b526a4052aa1624057657f91756
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-09-18 23:31:54 +0200

    133 -27b

--- solutions/task133.py
+++ solutions/task133.py
@@ -1,15 +1,14 @@
-def p(g):
- l=sum([[*r,0]for r in g],[]);w=len(g[0])+1
+def p(g,*l):
+ for a in g:l+=*a,0
+ w=len(g[0])+1
  for a in{*l}:
-  q=[l.index(a)]
-  for x in q:q+=[k for o in(-w,-1,1,w)if((k:=x+o)in q)==0<=k<len(l)>0<l[k]]
-  e,*f=sorted(s:=[l[k]for k in q],key=s.count)
-  if len(f)>1==len({*f}):v=e;z=q;n=min(k for k in q if l[k]==v)
+  q,*s=[l.index(a)],
+  for k in q:q+=[o for i in(-w,-1,1,w)if((o:=k+i)in q)==0<=o<len(l)>0<l[o]];s+=l[k],
+  if~-len(s)==s.count(a)>1:v,={*s}-{a};n=min(k for k in q if l[k]==v);z=q
  for a in{*l}-{v,0}:
-  q=[l.index(a)]
-  for x in q:q+=[k for o in(-w,-1,1,w)if((k:=x+o)in q)==0<=k<len(l)>0<l[k]]
-  o=min(k for k in q if l[k]==v)
-  u=l[o:o+4].count(v)
+  q,*s=[l.index(a)],
+  for k in q:q+=[o for i in(-w,-1,1,w)if((o:=k+i)in q)==0<=o<len(l)>0<l[o]];s+=l[k],
+  u=(s.count(v)^6)%6
   for k in z:
-   for i in range(u*u):g[o//w+(k//w-n//w)*u+i//u][o%w+(k%w-n%w)*u+i%u]+=a*(g[o//w+(k//w-n//w)*u+i//u][o%w+(k%w-n%w)*u+i%u]<1)
+   for i in range(u*u):x=min(k for k in q if l[k]==v)+k*u-n*u;g[x//w+i//u][x%w+i%u]+=a*(g[x//w+i//u][x%w+i%u]<1)
  return g
