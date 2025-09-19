b0366ef21f4a7450d5ff80f00f8637dbdb1a76fb
Mukundan314 <mukundan314@gmail.com>
2025-09-19 13:24:08 +0530

    226 -6b

--- solutions/task226.py
+++ solutions/task226.py
@@ -1,2 +1,2 @@
 E=enumerate
-p=lambda g:[[r[j]or(a:=sum(r[:j]+c[:i]))<1or(3>>(b:=sum(r[j:]+c[i:])))+2*(a==b==sum(r[j:]+c[:i]))for j,[*c]in E(zip(*g))]for i,r in E(g)]
+p=lambda g:[[r[j]or(a:=sum(r[:j]+c[:i]))<1or(3>>(b:=sum(r+c)-a))+2*(a==b==sum(r[j:]+c[:i]))for j,[*c]in E(zip(*g))]for i,r in E(g)]
