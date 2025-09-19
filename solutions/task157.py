efa90316b89a12a86fceeed2424acb274f3478ea
4atj <fourajbira16@gmail.com>
2025-09-19 14:44:52 +0100

    157 -2b (by fine-tuned Qwen2.5-Coder-3B-Instruct)

--- solutions/task157.py
+++ solutions/task157.py
@@ -1,5 +1,5 @@
 def p(g):
  for i in range(64):
   *h,=zip(*g);w=4-i//16;i%=16-w
-  for j in range(w*4*all(5in r for r in h[i:i+w])):m=max((-(1in sum(h[j:j+w],())),-len(q:={y.index(5)-q.index(min(q))for q,y in zip(h[j:j+w],h[i:i+w])}),min(q),j%w,j)for j in range(16-w));g[6+j%4-m[2]][m[4]+j//4]+=g[6+j%4][i+j//4]%4;g[6+j%4][i+j//4]=0
+  for j in range(w*4*all(5in r for r in h[i:i+w])):m=max((-(1in sum(h[j:j+w],())),-len(q:={y.index(5)-q.index(min(q))for q,y in zip(h[j:],h[i:i+w])}),min(q),j%w,j)for j in range(16-w));g[6+j%4-m[2]][m[4]+j//4]+=g[6+j%4][i+j//4]%4;g[6+j%4][i+j//4]=0
  return g
