c93c023b9792ee4be3b8b9565746d95a8e197f02
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-19 10:04:20 +0200

    133 -1b

--- solutions/task133.py
+++ solutions/task133.py
@@ -7,7 +7,6 @@ def p(g,*l):
  for a in{*l}-{v,0}:
   q,*s=[l.index(a)],
   for k in q:q+=[i+k for i in(~len(g[0]),-1,1,-~len(g[0]))if(i+k in q)==0<=i+k<len(l)>0<l[i+k]];s+=l[k],
-  u=(s.count(v)^6)%6
   for k in z:
-   for i in range(u*u):x=min(k for k in q if l[k]==v)+k*u-n*u;g[x//-~len(g[0])+i//u][x%-~len(g[0])+i%u]=g[x//-~len(g[0])+i//u][x%-~len(g[0])+i%u]or a
+   for i in range(s.count(v)):u=(s.count(v)^6)%6;x=min(k for k in q if l[k]==v)+k*u-n*u;g[x//-~len(g[0])+i//u][x%-~len(g[0])+i%u]=g[x//-~len(g[0])+i//u][x%-~len(g[0])+i%u]or a
  return g
