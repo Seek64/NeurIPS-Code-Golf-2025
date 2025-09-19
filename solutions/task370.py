e721dc3ef7e6af78c4f6fda0dacc2bad0e5cada8
Mukundan314 <mukundan314@gmail.com>
2025-09-19 23:29:37 +0530

    370 -14b

--- solutions/task370.py
+++ solutions/task370.py
@@ -1,6 +1,6 @@
 import re
 def p(g):
  for i in"."*4:
-  l=len(g);*g,=zip(*g[::-1]);f=sum(g,());i=f.index;x=i(0);y=i(v:=sum({*f})-f[0])
-  for _ in(y%l-x%l>0<l<y-x)*g:g=eval(re.sub("(?<=(0|%d).{%d})\d"%(v,(hash(f)%95==3)*87or(y-x)*3+2*(y-x+l-1)//l-3),"v",str(g)))
+  l=len(g);*g,=zip(*g[::-1]);f=sum(g,());i=f.index;x=i(0);y=i(v:=sum({*f})-f[0]);d=y-x
+  for _ in(d>y%l-x%l)*g:g=eval(re.sub(f"(?<=(0|{v}).{{{d*3+2*d//l-38*(hash(f)%95==3)-1}}})\d","v",str(g)))
  return g
