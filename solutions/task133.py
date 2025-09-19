092394afd9d2393e4fd748168eb946d1dc5fdbca
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-19 09:43:44 +0200

    133 -5b

--- solutions/task133.py
+++ solutions/task133.py
@@ -1,14 +1,13 @@
 def p(g,*l):
  for a in g:l+=*a,0
- w=len(g[0])+1
  for a in{*l}:
   q,*s=[l.index(a)],
-  for k in q:q+=[o for i in(-w,-1,1,w)if((o:=k+i)in q)==0<=o<len(l)>0<l[o]];s+=l[k],
+  for k in q:q+=[i+k for i in(~len(g[0]),-1,1,-~len(g[0]))if(i+k in q)==0<=i+k<len(l)>0<l[i+k]];s+=l[k],
   if~-len(s)==s.count(a)>1:v,={*s}-{a};n=min(k for k in q if l[k]==v);z=q
  for a in{*l}-{v,0}:
   q,*s=[l.index(a)],
-  for k in q:q+=[o for i in(-w,-1,1,w)if((o:=k+i)in q)==0<=o<len(l)>0<l[o]];s+=l[k],
+  for k in q:q+=[i+k for i in(~len(g[0]),-1,1,-~len(g[0]))if(i+k in q)==0<=i+k<len(l)>0<l[i+k]];s+=l[k],
   u=(s.count(v)^6)%6
   for k in z:
-   for i in range(u*u):x=min(k for k in q if l[k]==v)+k*u-n*u;g[x//w+i//u][x%w+i%u]+=a*(g[x//w+i//u][x%w+i%u]<1)
+   for i in range(u*u):x=min(k for k in q if l[k]==v)+k*u-n*u;g[x//-~len(g[0])+i//u][x%-~len(g[0])+i%u]=g[x//-~len(g[0])+i//u][x%-~len(g[0])+i%u]or a
  return g
