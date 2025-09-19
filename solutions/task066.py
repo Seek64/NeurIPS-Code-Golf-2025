bc4b77d95100843b2bf6525d1e8c7b694d0202f7
4atj <fourajbira16@gmail.com>
2025-09-19 13:56:52 +0100

    066 -4b (-1b by fine-tuned Qwen2.5-Coder-3B-Instruct)

--- solutions/task066.py
+++ solutions/task066.py
@@ -1,6 +1,6 @@
-def q(g,x,y,u,v,t=5):
- c=g=[x*1for x in g]
- while 0<x<len(g)-1>y>0==(c:=g[x+u][y+v]):x+=u;y+=v;g[x][y]=3
- if c>7>t:return q(g,x,y,v,u,t+1)or q(g,x,y,-v,-u,t+1)
- return(c==2)*g
-p=lambda g:max(q(g,x,y,u,v)for x in range(len(g))for y in range(len(g))for u in range(-1,2)for v in range(-1,2)if g[x][y]==3==g[x-u][y-v]>g[x+u][y+v])
+def p(g):
+ def p(g,x,y,u,v,t=5):
+  g=[x*1for x in g]
+  while 0<x<len(g)-1>y>0==(c:=g[x+u][y+v]):x+=u;y+=v;g[x][y]=3
+  return p(g,x,y,v,u,t+1)or p(g,x,y,-v,-u,t+1)if c>7>t else(c==2)*g
+ return max(p(g,x,y,u,v)for x in range(len(g))for y in range(len(g))for u in range(-1,2)for v in range(-1,2)if g[x][y]==3==g[x][y-v]>g[x+u][y+v])
