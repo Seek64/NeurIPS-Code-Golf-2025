1fcf3b1e664820201003b1f112e4a20f6ed20a66
4atj <fourajbira16@gmail.com>
2025-09-19 17:31:20 +0100

    324 -9b (post compression)

--- solutions/task324.py
+++ solutions/task324.py
@@ -2,7 +2,7 @@ def p(g):
  l=sum(g,t:=[]);f=sorted(l,key=l.count)[:2]
  for i in range(len(g)):
   for j in range(len(g[i])):
-   if g[i][j]in f:l[g[i][j]]=g[i][j];l[max(l,key=lambda l:([g[i][j-1],g[i][j-len(g[i])+1],g[i-1][j],g[i-len(g)+1][j]].count(l),-(len(g[i])*[l]in g)))]=g[i][j];f+=-i-j,;t+=i-j,
+   if g[i][j]in f:l[g[i][j]]=g[i][j];l[max(l,key=lambda l:[g[i][j-1],g[i][j-len(g[i])+1],g[i-1][j],g[i-len(g)+1][j]].count(l))]=g[i][j];f+=-i-j,;t+=i-j,
  for i in range(len(g)):
   for j in range(len(g[i])):
    if i-j in t or-i-j in f:g[i][j]=l[g[i][j]]
