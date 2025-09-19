b967935bccded13fd4b79829a678580315f1db42
Mukundan314 <mukundan314@gmail.com>
2025-09-19 22:55:03 +0530

    add 370

---
+++ solutions/task370.py
@@ -0,0 +1,6 @@
+import re
+def p(g):
+ for i in"."*4:
+  l=len(g);*g,=zip(*g[::-1]);f=sum(g,());i=f.index;x=i(0);y=i(v:=sum({*f})-f[0])
+  for _ in(y%l-x%l>0<l<y-x)*g:g=eval(re.sub("(?<=(0|%d).{%d})\d"%(v,(hash(f)%95==3)*87or(y-x)*3+2*(y-x+l-1)//l-3),"v",str(g)))
+ return g
