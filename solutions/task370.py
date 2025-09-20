e785f2bb0b32e2616ee5f95d5d3a56ae2e811a09
Mukundan314 <mukundan314@gmail.com>
2025-09-20 13:24:41 +0530

    370 -5b

--- solutions/task370.py
+++ solutions/task370.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec('s=f"{*zip(*g[::-1]),}#";i=s.rfind;d=i("0")-i(j:=min(s,key=i));d-=38*(d==126)+2;g[:]=eval(re.sub("(?=(.,.{%d})+0)."%d,j,s)*(d%(len(g)*3+1)<20)+s);'*4)or g
+p=lambda g:exec('s=f"{*zip(*g[::-1]),}";i=s.rfind;d=i("0")-i(j:=min(s,key=i))-2;g[:]=eval(re.sub("(?=(.,.{%d})+0)."%(d-38*(d==124)),j,s,-(d%-~len(3*g)>25)));'*4)or g
