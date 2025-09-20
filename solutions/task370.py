7008ac86c716da6488f77da051d0254638205b8a
Mukundan314 <mukundan314@gmail.com>
2025-09-20 08:12:53 +0530

    370 -10b

--- solutions/task370.py
+++ solutions/task370.py
@@ -1,6 +1,6 @@
 import re
 def p(g):
  for i in"."*4:
-  l=len(g);*g,=zip(*g[::-1]);f=sum(g,());i=f.index;x=i(0);y=i(v:=sum({*f})-f[0]);d=y-x
-  for _ in(d>y%l-x%l)*g:g=eval(re.sub(f"(?<=(0|{v}).{{{d*3+2*d//l-39**(d==40)}}})\d","v",str(g)))
+  l=len(g);*g,=zip(*g[::-1]);f=sum(g,());i=f.index;d=i(v:=sum({*f})-f[0])-i(0)
+  for _ in(d>d%l<6)*g:g=eval(re.sub(f"(?<=(0|{v}).{{{d*3+2*d//l-39**(d==40)}}})\d","v",str(g)))
  return g
