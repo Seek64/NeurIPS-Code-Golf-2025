a0bde627dffb463f3e72c8accfbc1e6af75653b8
4atj <fourajbira16@gmail.com>
2025-09-20 13:59:33 +0100

    037 -3b

--- solutions/task037.py
+++ solutions/task037.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:eval(re.sub(r"(?<=([^0]).{34})(?=(.{35})*\1)0",r"\1",str(g[10230:]or p(g*2)[::-1])))
+p=lambda g:eval(re.sub(r"(?<=(.).{34})(?=(.{35})*\1)0",r"\1",str(g[10230:]or p(g*2)[::-1])))
