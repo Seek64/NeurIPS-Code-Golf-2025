2eaab8c717d44144c49c959858c60294a832d012
Mukundan314 <mukundan314@gmail.com>
2025-09-21 14:51:02 +0530

    156 -6b

--- solutions/task156.py
+++ solutions/task156.py
@@ -1,2 +1,2 @@
 import re
-def p(g,S=re.subn):g,n=S(x:="(?<=4.{34})4(?=.{34}4)(?!.*0.{31}4)","a",str(g));g,m=S(x[:22],"3^a",g);a=(m<n)+1;return eval(g)
+def p(g,S=re.subn):g,n=S("(?<=4.{34})4(?=.{34}4)","a",str(g));g,m=S("(?=a.*0.{31}4)","3^",g);a=2-m*2//n;return eval(g)
