1f8b7da2da7457f2e062739c6806b45793791b78
Mukundan314 <mukundan314@gmail.com>
2025-09-21 16:27:52 +0530

    198 -1b

--- solutions/task198.py
+++ solutions/task198.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=-59,a='3|0':n*g or eval(re.sub(a,a[0],str([*zip(*p(g,n+1,r'4|0(?=, 4|[^)]*([^04]), \1)')[::-1])])))
+p=lambda g,n=-59,a='3|0':n*g or p(eval(re.sub(a,a[0],f"{*zip(*g[::-1]),}")),n+1,r'4|3(?=, 4|[^)]*([^34]), \1)')
