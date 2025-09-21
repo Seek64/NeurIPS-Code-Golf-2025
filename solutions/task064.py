251642789803afd8807bf4f02090c767b5f93a40
Mukundan314 <mukundan314@gmail.com>
2025-09-21 12:36:58 +0530

    064 -6b

--- solutions/task064.py
+++ solutions/task064.py
@@ -1,2 +1,2 @@
 import re
-def p(g):a,*_,b=sorted(f:=sum(g,[]),key=f.count);exec(f'g[:]=zip(*eval(re.sub("{a}, {b}(?=[^])]* [^{a}{b}])","a,a",str(g)))[::-1]);'*56);return g
+p=lambda g:[g:=eval(re.sub(f"{a,b}(?={e}*[^{a,b}])","a,a",f"{*zip(*g[::-1]),}"))for a,*e,b in[sorted(sum(g,[]),key=sum(g,g).count)]*56][-1]
