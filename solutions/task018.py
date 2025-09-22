e4d59b0ae8ca3e33f17f382e8be3c14ed36f9d21
4atj <fourajbira16@gmail.com>
2025-09-22 22:02:34 +0100

    fix 018

--- solutions/task018.py
+++ solutions/task018.py
@@ -1,11 +1,11 @@
 def p(e,*s):
  def p(z):
-  return *zip(*[((i,f,n),(i,-n,f))for i,n,f in z]),
+  return *zip(*[((i,m,n),(i,-n,m))for i,n,m in z]),
  d,*r=len(e[0]),
  for u in range(len(e)*d):
-  for i,n,f in(z:=[(t:=e[g:=u//d][o:=u%d],g,o)]*t):e[n][f]=0;z+=[(i,o,z)for u in range(9)if(o:=n+u//3-1)//len(e)|(z:=f+u%3-1)//d==0<(i:=e[o][z])]
-  r+=z*(len(z)<5*t);s+=p((i,n-g,f-o)for i,n,f in z)
+  for i,n,m in(z:=[(t:=e[g:=u//d][j:=u%d],g,j)]*t):e[n][m]=0;z+=[(i,o,z)for u in range(9)if(o:=n+u//3-1)//len(e)|(z:=m+u%3-1)//d==0<(i:=e[o][z])]
+  r+=z*(len(z)<5*t);s+=p((i,n-g,m-j)for i,n,m in z)
  for u in range(len(e)*d):
   for z in sum(map(p,s),s):
-   for i,n,f in(z:=[(i,n+u//d,f+u%d)for i,n,f in z])*(len({*r}&{*z})>2):e[n][f]=i
+   for i,n,m in(z:=[(i,n+u//d,m+u%d)for i,n,m in z])*(len({*r}&{*z})>2):e[n][m]=i
  return e
