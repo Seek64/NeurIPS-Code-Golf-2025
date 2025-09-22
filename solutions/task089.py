f62422c93bcb46eb9b31f07788208aed7ffd4368
lukegustafson <lukeg3@gmail.com>
2025-09-22 05:57:37 -0400

    089 -9b

--- solutions/task089.py
+++ solutions/task089.py
@@ -1,11 +1,1 @@
-def p(g):
- h=[g[x]*1for x in range(13)]
- for x in range(13):
-  for y in range(13):
-   for u in range(13):
-    for v in range(13):
-     if(x,y)!=(u,v)!=g[x][y]==g[u][v]in(2,3):
-      for i in range(-2,3):
-       for j in range(-2,3):
-        if-1<u+i<13>v+j*(g[u][v]>2or-1)>-1<g[u+i][v+j*(g[u][v]>2or-1)]in{g[u+i][v+j]for i in range(-1,2)for j in range(-1,2)if-1<u+i<13>v+j>-1}-{0,2,3}:h[x+i][y+j]=g[u+i][v+j*(g[u][v]>2or-1)]
- return h
+p=lambda q:[[[*[q[n-l][y-j*(q[n][y]>2or-1)]for l in range(-2,3)for j in range(-2,3)if-1<r+l<13>a+j>-1<q[r+l][a+j]in(2,3)for n in range(13)for y in range(13)if(n,y)!=(r+l,a+j)!=-1<n-l<13>y-j*(q[n][y]>2or-1)>-1<q[n][y]==q[r+l][a+j]!=q[n-l][y-j*(q[n][y]>2or-1)]in{q[n-l][y-j]for l in range(-1,2)for j in range(-1,2)if n-l<13>y-j}-{0}],q[r][a]][0]for a in range(13)]for r in range(13)]
