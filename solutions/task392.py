7818254db1f1b3f20eb26ebb737e4eaceb1cfa5d
lukegustafson <lukeg3@gmail.com>
2025-09-22 14:26:07 -0400

    392 -3b

--- solutions/task392.py
+++ solutions/task392.py
@@ -1,2 +1,2 @@
 R=range(10);m=max
-p=lambda g:[[[w:=m(m(g)),5,5][m(6*g[Y][X]-m(abs(x-X),abs(y-Y))for X in R for Y in R)%(3-(f'0, {w}, '*2in'%s'%g))]for x in R]for y in R]
+p=lambda g:[[[5,w:=m(m(g))][m(6*g[Y][X]+~m(x-X,X-x,y-Y,Y-y)for X in R for Y in R)%((f'0, {w}, '*2in'%s'%g)-3)]for x in R]for y in R]
