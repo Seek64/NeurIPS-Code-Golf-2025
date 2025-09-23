c9a9578056034ce89c5b52080274d9391ad6a766
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-23 13:15:04 +0200

    233 -15b (wip, simple restructure)

--- solutions/task233.py
+++ solutions/task233.py
@@ -1,8 +1,18 @@
-def p(u):
- i=range(9);c={};f=lambda a:[a(p,s,[u[p+a//3][s+a%3]for a in i])for p in range(len(u)-2)for s in range(len(u[0])-2)]
- def p(p,s,n):
-  if(0 in n)<1<len(r:={*n}):
-   n=tuple(a==2for a in n);k={n:sum(r)-2}
-   for a in range(4):c[n]=k;n=tuple(n[a%3*3+2-a//3]for a in i)
-   for a in i:u[p+a//3][s+a%3]=0
- f(p);u=[*map(list,zip(*filter(any,[*map(list,zip(*filter(any,u)))])))];[f(lambda p,s,n:(t:=c.get(n:=tuple(a==0for a in n)))and(a or n in t)and(r:=t.popitem()[1],[u[p+a//3].__setitem__(s+a%3,(r,2)[n[a]])for a in i]))for a in range(2)];return u
+def p(g):
+ R=range;S={}
+ for i in range(len(g)-2):
+  for j in range(len(g[0])-2):
+   m=[g[i+k//3][j+k%3]for k in range(9)]
+   if(0 in m)<1<len(C:={*m}):
+    K=tuple(x==2for x in m);D={K:sum(C)-2}
+    for k in range(9):S[K]=D;K=tuple(K[k%3*3+2-k//3]for k in range(9));g[i+k//3][j+k%3]=0
+ g=[*map(list,zip(*filter(any,[*map(list,zip(*filter(any,g)))])))]
+ for c in(0,1):
+  for i in range(len(g)-2):
+   for j in range(len(g[0])-2):
+    m=[g[i+k//3][j+k%3]for k in range(9)]
+    P=tuple(x<1for x in m);D=S.get(P)
+    if D and(c or P in D):
+     v=D.popitem()[1]
+     for k in range(9):g[i+k//3][j+k%3]=(v,2)[P[k]]
+ return g
