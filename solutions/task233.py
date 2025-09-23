cdb08f0d0565248b3fd43c0013e3445d2da52925
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-23 16:45:39 +0200

    233 -31b

--- solutions/task233.py
+++ solutions/task233.py
@@ -1,18 +1,23 @@
-def p(g):
- R=range;S={}
+def p(g,S={}):
  for i in range(len(g)-2):
   for j in range(len(g[0])-2):
    m=[g[i+k//3][j+k%3]for k in range(9)]
-   if(0 in m)<1<len(C:={*m}):
-    K=tuple(x==2for x in m);D={K:sum(C)-2}
-    for k in range(9):S[K]=D;K=tuple(K[k%3*3+2-k//3]for k in range(9));g[i+k//3][j+k%3]=0
- g=[*map(list,zip(*filter(any,[*map(list,zip(*filter(any,g)))])))]
- for c in(0,1):
-  for i in range(len(g)-2):
-   for j in range(len(g[0])-2):
-    m=[g[i+k//3][j+k%3]for k in range(9)]
-    P=tuple(x<1for x in m);D=S.get(P)
-    if D and(c or P in D):
-     v=D.popitem()[1]
-     for k in range(9):g[i+k//3][j+k%3]=(v,2)[P[k]]
+   P=*[x==2for x in m],;D={P:sum(C:={*m})-2}
+   if(0 in m)<1<len(C):
+    for k in range(9):S[P]=D;P=*[P[k%3*3+2-k//3]for k in range(9)],;g[i+k//3][j+k%3]=0
+ g=[*map(list,zip(*filter(sum,zip(*filter(sum,g)))))]
+ for i in range(len(g)-2):
+  for j in range(len(g[0])-2):
+   m=[g[i+k//3][j+k%3]for k in range(9)]
+   P=*[x!=2for x in m],;D=S.get(P)
+   if D and P in D:
+    v=D.popitem()[1]
+    for k in range(9):g[i+k//3][j+k%3]=(v,2)[P[k]]
+ for i in range(len(g)-2):
+  for j in range(len(g[0])-2):
+   m=[g[i+k//3][j+k%3]for k in range(9)]
+   P=*[x!=2for x in m],;D=S.get(P)
+   if D:
+    v=D.popitem()[1]
+    for k in range(9):g[i+k//3][j+k%3]=(v,2)[P[k]]
  return g
