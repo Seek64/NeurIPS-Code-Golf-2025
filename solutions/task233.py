72ba7cf155b58cb271efd2142b2530de8111d081
4atj <fourajbira16@gmail.com>
2025-09-23 16:08:40 +0100

    233 -19b (post compression)

--- solutions/task233.py
+++ solutions/task233.py
@@ -1,23 +1,26 @@
-def p(g,S={}):
- for i in range(len(g)-2):
-  for j in range(len(g[0])-2):
-   m=[g[i+k//3][j+k%3]for k in range(9)]
-   P=*[x==2for x in m],;D={P:sum(C:={*m})-2}
-   if(0 in m)<1<len(C):
-    for k in range(9):S[P]=D;P=*[P[k%3*3+2-k//3]for k in range(9)],;g[i+k//3][j+k%3]=0
- g=[*map(list,zip(*filter(sum,zip(*filter(sum,g)))))]
- for i in range(len(g)-2):
-  for j in range(len(g[0])-2):
-   m=[g[i+k//3][j+k%3]for k in range(9)]
-   P=*[x!=2for x in m],;D=S.get(P)
-   if D and P in D:
-    v=D.popitem()[1]
-    for k in range(9):g[i+k//3][j+k%3]=(v,2)[P[k]]
- for i in range(len(g)-2):
-  for j in range(len(g[0])-2):
-   m=[g[i+k//3][j+k%3]for k in range(9)]
-   P=*[x!=2for x in m],;D=S.get(P)
-   if D:
-    v=D.popitem()[1]
-    for k in range(9):g[i+k//3][j+k%3]=(v,2)[P[k]]
+def p(g):
+ e={}
+ for q in range(len(g)-2):
+  for u in range(len(g[0])-2):
+   r=[g[q+f//3][u+f%3]for f in range(9)]
+   i=*(g<1for g in r),
+   if(0 in r)<1<len({*r}):
+    i=*(g==2for g in r),;p={i:sum({*r})-2}
+    for f in range(9):e[i]=p;i=*(i[2+f%3*3-f//3]for f in range(9)),;g[q+f//3][u+f%3]=0
+ g=[[*g]for g in zip(*g)if any(g)]
+ g=[[*g]for g in zip(*g)if any(g)]
+ for q in range(len(g)-2):
+  for u in range(len(g[0])-2):
+   r=[g[q+f//3][u+f%3]for f in range(9)]
+   i=*(g<1for g in r),
+   if e.get(i,{}).get(i,{}):
+    l=e.get(i,{}).popitem()[1]
+    for f in range(9):g[q+f//3][u+f%3]=i[f]*2 or l
+ for q in range(len(g)-2):
+  for u in range(len(g[0])-2):
+   r=[g[q+f//3][u+f%3]for f in range(9)]
+   i=*(g<1for g in r),
+   if e.get(i,{}):
+    l=e.get(i,{}).popitem()[1]
+    for f in range(9):g[q+f//3][u+f%3]=i[f]*2 or l
  return g
