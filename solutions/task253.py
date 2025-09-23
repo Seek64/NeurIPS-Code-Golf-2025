9d61555e30fe838a2063c5593f26ed7191f85f93
lukegustafson <lukeg3@gmail.com>
2025-09-23 02:17:21 -0400

    253 -1b

--- solutions/task253.py
+++ solutions/task253.py
@@ -1,2 +1,2 @@
 F=0,1,2,3
-p=lambda g:[[max(min(g[p//12+v//2][p%12+v%2]for v in F if x*y%3|v^y&6^x//2^3)for p in range(144))for x in F]for y in F]
+p=lambda g:[[max(min(g[p//12+v//2][p%12+v%2]for v in F if~y&2^(2>x)^v|x*y%3)for p in range(144))for x in F]for y in F]
