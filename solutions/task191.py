fdfe9207185fc7a94625aa1d7999b2405357d8ef
lukegustafson <lukeg3@gmail.com>
2025-09-24 07:31:24 -0400

    191 -4b

--- solutions/task191.py
+++ solutions/task191.py
@@ -1,7 +1,8 @@
 import re
 def p(e):
  def p(e):
-  return'(%s)'%re.sub('[^%d]'%(4-(']'in e[0])),'.',e[0])
- *e,=[0]*25,*[[0,*r,0]for r in e],[0]*25;z=re.sub(*'10',re.sub('[^1]+',p,re.search('1.*1','(%s)'%e)[0]))
+  return'(%s)'%re.sub('[^%d]'%(4-(')'in e[0])),'.',e[0])
+ for r in[1,-1,1]*4:*e,=zip([0]*len(e[0]),*e[::r])
+ z=re.sub(*'10',re.sub('[^1]+',p,re.search('1.*1','(%s)'%e)[0]))
  for r in[1,-1,1]*4:*e,=zip(*eval('1'.join(re.split(z,'(%s)'%e[::r]))))
- return[r[1:-1]for r in e[1:-1]]
+ return[r[3:-3]for r in e[3:-3]]
