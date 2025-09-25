96cda5be68dbc31d1517f4d90cb9d6653021ec9c
lukegustafson <lukeg3@gmail.com>
2025-09-24 22:50:00 -0400

    051 -1b

--- solutions/task051.py
+++ solutions/task051.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub(r"0(?=[^)]*([^0]), ((?!\1|0).), 0)",r"\2",f"{*zip(*g[::-1]),}"))for _ in g][3]
+p=lambda g:[g:=eval(re.sub(r"0(?=[^)]*?([^0]), ((?!\1).), 0)",r"\2",f"{*zip(*g[::-1]),}"))for _ in g][3]
