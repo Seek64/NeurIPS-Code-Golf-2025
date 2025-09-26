77dec1d5f5156f4d887fbd78758ee35004010adc
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-09-26 10:17:27 +0200

    101 -42b (new approach similar to 133, wip)

--- solutions/task101.py
+++ solutions/task101.py
@@ -1,11 +1,14 @@
-import math
-e=enumerate
-def p(g,m=3):
- P=[(c,x,y)for x,r in e(g)for y,c in e(r)if c];R=max(B:={p for p in P for b in P if b[0]<2>math.dist(b,p)})
- for q in P:
-  G=[m*[0]+r for r in g*9];n=len(B)*m
-  for c,x,y in B:
-   M=s=q[1]+m*(x-R[1]);t=q[2]-m*(~y+R[2])
-   while s<M+m:n-=G[s][t:t+m]==[c&2]*m;G[s][t:t+m]=[c|8]*m;s+=1
-  if(q!=R*m)*n<1:g=[G[i][m:]for i,_ in e(g)]
- return[[c&7for c in r]for r in g]*(2-m)or p(g,m-1)
+def p(g):
+ l=sum([[*x,0,0]for x in g],[])
+ w=len(g[0])+2
+ q=[l.index(1)]
+ for x in q:q+=[x+i for i in(-w,-1,1,w)if(x+i in q)==0<=x+i<len(l)>0<l[x+i]]
+ a=min(x for x in q if l[x]>1);s=q
+ for i in range(len(l)):
+  q=[i]
+  for x in q:q+=[x+i for i in(-w,-1,1,w)if(x+i in q)==0<=x+i<len(l)>0<l[x+i]]
+  if all(l[x]>1for x in q):
+   u=6+26%len(q)>>2
+   for j in s:
+    for x in range(u*u):l[i+(j-a)*u+x//u*w+x%u]=l[j]
+ return[l[i*w:][:w-2]for i in range(len(g))]
