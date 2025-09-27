78b1fd43a5a9a5b77f419cfc037ac9105c6ef08f
lukegustafson <lukeg3@gmail.com>
2025-09-27 09:21:00 -0400

    239 -2b

--- solutions/task239.py
+++ solutions/task239.py
@@ -1,1 +1,1 @@
-p=lambda g:[*filter(any,zip(*sorted([c:=-sum(g,g).count(e)]+[e]*-c+[0]*99for e in{*sum(g,[])})))][1:]
+p=lambda g:[s:=sum(g,[]),*filter(any,zip(*sorted([c:=-s.count(e)]+[e]*-c+[0]*99for e in{*s})))][2:]
