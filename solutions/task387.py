345ea9114c75021dddae385ba49d18bf0019596e
4atj <fourajbira16@gmail.com>
2025-09-28 05:38:54 +0100

    387 -6b

--- solutions/task387.py
+++ solutions/task387.py
@@ -1,6 +1,6 @@
-def p(l):
- for r in range(4):
-  n=len(l[0]);m=len(l);e=sum(l,[]);r,l={e.index(l):0for l in e if l%5}
-  while l-r>3:e[r:=r+2]=e[l:=l-2]=5
-  l=[e[r-n::-n]for r in range(n)]
- return[[l[r][f]or len(n:={*e}-{*(3*sum((l[:1]+l)[r:r+3],[0]))[f+2::m],5})%2*max(n)for f in range(m)]for r in range(n)]
+def p(o):
+ for q in range(4):
+  r=sum(o,[]);q,p,q,p,=[r.index(q)for q in r if q%5]
+  while q-p>3:p+=2;q-=2;r[p]=r[q]=5
+  o=[r[q-len(o[0])::-len(o[0])]for q in range(len(o[0]))]
+ return[[o[q][n]or len(p:={*r}-{*(3*sum((o[:1]+o)[q:q+3],[0]))[n+2::len(o[0])],5})%2*max(p)for n in range(len(o[0]))]for q in range(len(o))]
