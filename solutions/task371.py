d5f6ce5c4f262408f76ea65d117d5de9831546f4
Mukundan314 <mukundan314@gmail.com>
2025-09-30 15:36:06 +0530

    371 -1b

--- solutions/task371.py
+++ solutions/task371.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,n=3:g*-n or[*zip(*eval(re.sub(r'(1(.*)).{6}(0\2(1))|, 7,',r'\1+3,3\4%8,3+\3',str(p(g,n-1)))))]
+p=lambda g,n=3:g*-n or[*zip(*eval(re.sub(r'(1(.*)).{6}(0\2(1))|, 31,',r'\1+3,3\4,3+\3',str(p(g,n-1)))))]
