42327323fcd30ca12fe2594f0daac41c010bce98
Mukundan314 <mukundan314@gmail.com>
2025-09-30 16:10:47 +0530

    182 -4b

--- solutions/task182.py
+++ solutions/task182.py
@@ -1,2 +1,2 @@
 from re import*
-p=lambda g:[g:=eval(sub(sub("2|3","1","(.{47})".join(a:=findall(".{46}(.{15})5"*5+".{46}5",s:=str(g))[0])),"\%d ".join(a)%(1,2,3,4),s))for _ in g][3]
+p=lambda g,h=0:eval(sub(sub("2|3","1","(.{47})".join(a:=findall(".{46}(.{15})5"*5+".{46}5",s:=str(h or p(g,g)))[0])),"\%d ".join(a)%(1,2,3,4),s))
