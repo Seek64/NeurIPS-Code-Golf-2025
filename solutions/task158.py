e463417a8675580d8f61f644396a734896634327
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-10-02 22:57:04 +0200

    158 -5b (post compression)

--- solutions/task158.py
+++ solutions/task158.py
@@ -1,15 +1,15 @@
 def p(g):
- for l in(3,2,1):
-  for e in(-1,1):
-   for i in range(len(g)*2-3*l+1):
-    for j in range(len(g[0])-3*l+1):
-     m=[r[j:j+3*l][::e]for r in(g+g[::-1])[i:i+3*l]]
-     if len({*f"{m}"})>7>0<m[0][0]!=m[-1][-1]:q=m
- for l in(3,2,1):
-  for e in(-1,1):
-   for i in range(len(g)*2-3*l+1):
-    for j in range(len(g[0])-3*l+1):
-     m=[r[j:j+3*l][::e]for r in(g+g[::-1])[i:i+3*l]]
-     if m[0][:l]==q[0][:1]*l!=m[-1][-l:]==m[-l][-l:]==q[-1][-1:]*l!=m[0][l]==m[-1][~l]:
-      for k in range(3*l*3*l):(g+g[::-1])[i+k//(3*l)][j+k%(3*l)]=q[k//(3*l)//l][::e][k%(3*l)//l]
+ for i in range(12):
+  l=3-i//4;g=[[*g]for g in zip(*g[::-1])]
+  for i in range(len(g)-3*l+1):
+   for j in range(len(g[0])-3*l+1):
+    m=[g[j:j+3*l]for g in g[i:i+3*l]]
+    if m[0][0]!=m[-1][-1]>0<7<len({*f"{m}"}):q=m
+ for i in range(12):
+  l=3-i//4;g=[[*g]for g in zip(*g[::-1])]
+  for i in range(len(g)-3*l+1):
+   for j in range(len(g[0])-3*l+1):
+    m=[g[j:j+3*l]for g in g[i:i+3*l]]
+    if m[0][:l]==q[0][:1]*l!=m[-1][-l:]==m[-l][-l:]==q[-1][-1:]*l!=m[0][l]==m[-1][~l]:
+     for k in range(3*l*3*l):g[i+k//(3*l)][j+k%(3*l)]=q[k//(3*l)//l][k%(3*l)//l]
  return g
