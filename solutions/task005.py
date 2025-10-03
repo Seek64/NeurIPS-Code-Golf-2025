33dbafd922704e9221c72b4add9de4bf0b8a3d08
Mukundan314 <mukundan314@gmail.com>
2025-10-03 08:00:10 +0530

    005 -5b

--- solutions/task005.py
+++ solutions/task005.py
@@ -1,5 +1,2 @@
 import re
-def p(g):
- c=max("987643251",key=str(g).count)
- for n in[11,271,0]*4:s="[^#]"*n;g=eval(re.sub(f'(?=(\d{s})*{c}.*([1-9]){s+c})0',r"\2",f"{*zip(*g[::-1]),}#"*2))
- return g
+def p(g):c=max("987643251",key=str(g).count);return[g:=eval(re.sub(f'(?=(\d{"."*n})*{c}.*([^0]){"."*n+c})0',r"\2",f"{*zip(*g[::-1]),}#"*2))for n in[11,271,0]*4][-1]
