85ddf196697047793a8be77135faddffb0321c55
lukegustafson <lukeg3@gmail.com>
2025-10-03 22:20:11 -0400

    206 -14b

--- solutions/task206.py
+++ solutions/task206.py
@@ -1,4 +1,4 @@
 def p(g):
- t=sum(g,[]).index(5);l=len(g[0])
- for a in[[x for*c,x in zip(*g,r)if{*c}-{0,5}]for r in g if{*r}-{0,5}]:g[t//l-1][t%l-1:t%l+2]=a;t+=l
+ t=sum(g,[]).index(5);l=len(g[0]);q=lambda h:[r for r in zip(*h)if{*r}-{0,5}]
+ for g[t//l-1][t%l-1:t%l+2]in q(q(g)):t+=l
  return g
