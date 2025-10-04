afe29ee2bc0863134fe78c009a07756ca6aa63ed
lukegustafson <lukeg3@gmail.com>
2025-10-04 09:06:14 -0400

    059 -11b

--- solutions/task059.py
+++ solutions/task059.py
@@ -1,2 +1,1 @@
-R=range(11)
-p=lambda g:[[(g[i][j]==5)*5or(max(l:=[sum(sum(r[i%3*4:][:3])for r in g[i//3*4:][:3])for i in R])==l[i//4*3+j//4])*sum({*sum(g,[-5])})for j in R]for i in R]
+R=range(11);p=lambda g,n=36,o=0:[[(g[i][j]==5)*5or(sum(S:=sum([r[j&12:][:3]for r in g[i&12:][:3]],[]))>n!=[o:=1])*max(S)for j in R]for i in R]*o or p(g,n-1)
