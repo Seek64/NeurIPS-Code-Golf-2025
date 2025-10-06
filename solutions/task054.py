1ada77f18de9a9c22428b2ff0ad934662d9f3e15
lukegustafson <lukeg3@gmail.com>
2025-10-06 11:01:53 -0400

    054 -9b

--- solutions/task054.py
+++ solutions/task054.py
@@ -1,18 +1,17 @@
-def p(g):
- h=[r*1for r in g]
- for i in range(28):
-  for j in range(28):
-   if g[i][j+1]==g[i][j-1]!=g[i][j]!=g[i+1][j]==g[i-1][j]>0<len({g[i+1][j],g[i][j+1],g[i+1][j+1]})>1:
-    for k in range(28):
-     for l in range(28):
-      if g[i][j]==g[k][l]:
-       for m in range(-1,2):
-        for n in range(-1,2):
-         if g[i+m][j+n]!=g[i+1][j+2]:
-          o=1;h[k+m*o][l+n*o]=g[i+m][j+n];o+=1
-          if g[i][j]!=g[i+m*o][j+n*o]!=g[i+1][j+2]:
-           while g[k+m*o][l+n*o]!=g[i+1][j+2]:h[k+m*o][l+n*o]=g[i+m][j+n];o+=1
-    for m in range(-2,3):
-     for n in range(-2,3):
-      h[i+m][j+n]=g[i+1][j+2]
-    return h
+def p(m):
+ o=[r*1for r in m]
+ for p in range(28):
+  for k in range(28):
+   if m[p][k+1]==m[p][k-1]!=m[p][k]!=m[p+1][k]==m[p-1][k]!={m[p+1][k],m[p][k+1],m[p+1][k+1]}!={m[p+1][k]}:
+    for d in range(28):
+     for b in range(28):
+      if m[p][k]==m[d][b]:
+       for l in range(-1,2):
+        for h in range(-1,2):
+         if m[p+l][k+h]!=m[p+1][k+2]:
+          g=1;o[d+l*g][b+h*g]=m[p+l][k+h];g+=1
+          if m[p][k]!=m[p+l*g][k+h*g]!=m[p+1][k+2]:
+           while m[d+l*g][b+h*g]!=m[p+1][k+2]:o[d+l*g][b+h*g]=m[p+l][k+h];g+=1
+    for l in range(-2,3):
+     for h in range(-2,3):o[p+l][k+h]=m[p+1][k+2]
+    return o
