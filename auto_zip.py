0c39755a803ead2cc2e81f4fbfaf3bb78ca0201c
4atj <fourajbira16@gmail.com>
2025-10-07 22:25:57 +0100

    trying different seeds (ah, not worth it) -3b

--- auto_zip.py
+++ auto_zip.py
@@ -1,5 +1,7 @@
-import os, golf_utils, zipfile
-from multiprocessing import Pool, cpu_count
+from typing import MutableMapping
+
+import os, golf_utils, zipfile, json
+from multiprocessing import Pool, cpu_count, Manager
 
 source = "solutions"
 submission = "submission"
@@ -7,7 +9,8 @@ submission = "submission"
 min_pack_length = 200
 os.makedirs(submission, exist_ok=True)
 
-def process_task(task_num: int):
+
+def process_task(task_num: int, zopfli_settings: MutableMapping[int, dict[str, int]]):
     path_in = f"{source}/task{task_num:03d}.py"
     path_out = f"{submission}/task{task_num:03d}.py"
 
@@ -19,11 +22,25 @@ def process_task(task_num: int):
 
     improvement = 0
     if len(task_src) >= min_pack_length:
-        zipped_src = golf_utils.pack(task_src)
+        if task_num in zopfli_settings:
+            best_zopfli_seed = zopfli_settings[task_num]["seed"]
+            best_zopfli_iters = zopfli_settings[task_num]["iters"]
+        else:
+            best_zopfli_seed, best_zopfli_iters = golf_utils.find_best_zopfli_settings(
+                task_src
+            )
+            zopfli_settings[task_num] = {
+                "seed": best_zopfli_seed,
+                "iters": best_zopfli_iters,
+            }
+
+        zipped_src = golf_utils.pack(task_src, best_zopfli_seed, best_zopfli_iters)
         difference = len(task_src) - len(zipped_src)
         if difference > 0:
             improvement = difference
-            print(f"Task {task_num:03d}: {len(task_src)} -> {len(zipped_src)} (+{improvement})")
+            print(
+                f"Task {task_num:03d}: {len(task_src)} -> {len(zipped_src)} (+{improvement})"
+            )
             task_src = zipped_src
 
     with open(path_out, "wb") as task_out:
@@ -31,12 +48,28 @@ def process_task(task_num: int):
 
     return improvement
 
+
 if __name__ == "__main__":
-    with Pool(cpu_count()) as pool:
-        improvements = pool.map(process_task, range(1, 401))
+    manager = Manager()
+    zopfli_settings: MutableMapping[int, dict[str, int]] = manager.dict()
+
+    try:
+        with open("zopfli_settings.json", "r") as f:
+            for k, v in json.load(f).items():
+                zopfli_settings[int(k)] = v
+    except FileNotFoundError:
+        pass
+
+    with Pool(4) as pool:
+        improvements = pool.starmap(
+            process_task, [(task_num, zopfli_settings) for task_num in range(1, 401)]
+        )
 
     total_save = sum(improvements)
     print(f"Saved {total_save}b with zlib")
+
+    with open("zopfli_settings.json", "w") as f:
+        json.dump(dict(zopfli_settings), f)
 
     with zipfile.ZipFile(f"{submission}.zip", "w") as zipf:
         for task_num in range(1, 401):
--- golf_utils.py
+++ golf_utils.py
@@ -1,10 +1,13 @@
 import zopfli.zlib
 import deflate
+import random
 
 from reencoder import reencode
 
+random.seed(0)
+ZOPFLI_SEEDS = [2 << 32 | 1] + [random.getrandbits(64) for _ in range(256)]
 ZOPFLI_ITERS = [2048]
-DEFLATE_LEVELS = range(7, 13)
+DEFLATE_LEVELS = list(range(7, 13))
 DELIMS = [b"'", b'"']
 
 # ZOPFLI_ITERS = [1 << i for i in range(10)]
@@ -32,15 +35,29 @@ def sanitize(b_in: bytes, delim: bytes) -> bytes:
     return bytes(b_out)
 
 
-def compress(src: bytes) -> bytes:
+def find_best_zopfli_settings(src: bytes) -> tuple[int, int]:
+    best_settings = ZOPFLI_SEEDS[0], ZOPFLI_ITERS[0]
+    best_length = len(src)
+
+    for seed in ZOPFLI_SEEDS:
+        for i in ZOPFLI_ITERS:
+            length = len(pack(src, zopfli_seed=seed, zopfli_iters=i, deflate_levels=[]))  # type: ignore[reportUnknownArgumentType]
+
+            if length < best_length:
+                best_settings = seed, i
+                best_length = length
+
+    return best_settings
+
+
+def compress(src: bytes, zopfli_seed: int = ZOPFLI_SEEDS[0], zopfli_iters: int = ZOPFLI_ITERS[0], deflate_levels: list[int] = DEFLATE_LEVELS) -> bytes:
     """Given a bytes object, returns a zlib compressed literal"""
-    compressed: list[bytes | bytearray] = []
+    compressed: list[bytes] = []
 
     # Zopfli attempts
-    for i in ZOPFLI_ITERS:
-        compressed.append(zopfli.zlib.compress(src, numiterations=i)[2:-4])  # type: ignore[reportUnknownArgumentType]
+    compressed.append(zopfli.zlib.compress(src, numiterations=zopfli_iters, seed=zopfli_seed)[2:-4])  # type: ignore[reportUnknownArgumentType]
     # deflate (libdeflate) attempts
-    for level in DEFLATE_LEVELS:
+    for level in deflate_levels:
         compressed.append(deflate.deflate_compress(src, compresslevel=level))  # type: ignore[reportUnknownArgumentType]
 
     literals: list[bytes] = []
@@ -51,24 +68,25 @@ def compress(src: bytes) -> bytes:
     return min(literals, key=len)
 
 
-def pack(src: bytes) -> bytes:
+def pack(src: bytes, zopfli_seed: int = ZOPFLI_SEEDS[0], zopfli_iters: int = ZOPFLI_ITERS[0], deflate_levels: list[int] = DEFLATE_LEVELS) -> bytes:
     """Given a python program as a bytes object, returns a possibly shorter python program"""
-    codes = []
-
-    codes.append(
-        b"#coding:L1\nimport zlib\nexec(zlib.decompress(bytes("
-        + compress(src)
-        + b',"L1"),~9))'
-    )
-
     if src.startswith(b"import"):
         imports = src.split()[1]
-        codes.append(
+        packed = (
             b"#coding:L1\nimport zlib,"
             + imports
             + b"\nexec(zlib.decompress(bytes("
-            + compress(src[len(imports) + 8:])
+            + compress(src[len(imports) + 8:], zopfli_seed, zopfli_iters, deflate_levels)
+            + b',"L1"),~9))'
+        )
+    else:
+        packed = (
+            b"#coding:L1\nimport zlib\nexec(zlib.decompress(bytes("
+            + compress(src, zopfli_seed, zopfli_iters, deflate_levels)
             + b',"L1"),~9))'
         )
 
-    return min(codes, key=len)
+    if len(packed) > len(src):
+        return src
+    
+    return packed
