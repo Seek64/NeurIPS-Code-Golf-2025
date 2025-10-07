aa594835861c195db367f766d55c813e2cdf9a90
4atj <fourajbira16@gmail.com>
2025-10-07 22:41:21 +0100

    158 -4b (post compression)

--- solutions/task158.py
+++ solutions/task158.py
@@ -1,15 +1,15 @@
-def p(g):
- for k in range(12):
-  l=3-k//4;g=[[*k] for k in zip(*g[::-1])]
-  for i in range(len(g)+1-3*l):
-   for j in range(len(g[0])+1-3*l):
-    m=[g[i+k//(3*l)][j+k%(3*l)] for k in range(3*l*3*l)]
-    if m[0]!=m[8]!=3<len({*m}):q=m
- for k in range(12):
-  l=3-k//4;g=[[*k] for k in zip(*g[::-1])]
-  for i in range(len(g)+1-3*l):
-   for j in range(len(g[0])+1-3*l):
-    m=[g[i+k//(3*l)][j+k%(3*l)] for k in range(3*l*3*l)]
-    if m[:l]==m[l*3*l-3*l:][:l]==q[:1]*l!=m[-l:]==q[8:]*l!=m[~l]==m[l]:
-     for k in range(3*l*3*l):g[i+k//(3*l)][j+k%(3*l)]=q[k//(3*l)//l*3+k%(3*l)//l]
- return g
+def p(e):
+ for u in range(12):
+  r=3-u//4;e=[[*u] for u in zip(*e[::-1])]
+  for n in range(len(e)+1-3*r):
+   for g in range(len(e[0])+1-3*r):
+    u=[e[n+u//(3*r)][g+u%(3*r)] for u in range(3*r*3*r)]
+    if u[0]!=u[8]!=3<len({*u}):d=u
+ for u in range(12):
+  r=3-u//4;e=[[*u] for u in zip(*e[::-1])]
+  for n in range(len(e)+1-3*r):
+   for g in range(len(e[0])+1-3*r):
+    u=[e[n+u//(3*r)][g+u%(3*r)] for u in range(3*r*3*r)]
+    if u[:r]==u[r*3*r-3*r:][:r]==d[:1]*r!=u[-r:]==d[8:]*r!=u[~r]==u[r]:
+     for u in range(3*r*3*r):e[n+u//(3*r)][g+u%(3*r)]=d[u//(3*r)//r*3+u%(3*r)//r]
+ return e
