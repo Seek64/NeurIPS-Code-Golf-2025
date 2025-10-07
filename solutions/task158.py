d463620f04bbc8ef5973323985f6b5fd8c68e67a
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-10-07 10:24:06 +0200

    158 -5b (post compression)

--- solutions/task158.py
+++ solutions/task158.py
@@ -1,15 +1,15 @@
 def p(g):
  for k in range(12):
-  l=3-k//4;g=[[*k]for k in zip(*g[::-1])]
-  for i in range(len(g)-3*l+1):
-   for j in range(len(g[0])-3*l+1):
-    m=[g[i+k//(3*l)][j+k%(3*l)]for k in range(3*l*3*l)]
+  l=3-k//4;g=[[*k] for k in zip(*g[::-1])]
+  for i in range(len(g)+1-3*l):
+   for j in range(len(g[0])+1-3*l):
+    m=[g[i+k//(3*l)][j+k%(3*l)] for k in range(3*l*3*l)]
     if m[0]!=m[8]!=3<len({*m}):q=m
  for k in range(12):
-  l=3-k//4;g=[[*k]for k in zip(*g[::-1])]
-  for i in range(len(g)-3*l+1):
-   for j in range(len(g[0])-3*l+1):
-    m=[g[i+k//(3*l)][j+k%(3*l)]for k in range(3*l*3*l)]
-    if m[:l]==q[:1]*l!=m[-l:]==m[-l*3*l+3*l-l:][:l]==q[8:]*l!=m[~l]==m[l]:
+  l=3-k//4;g=[[*k] for k in zip(*g[::-1])]
+  for i in range(len(g)+1-3*l):
+   for j in range(len(g[0])+1-3*l):
+    m=[g[i+k//(3*l)][j+k%(3*l)] for k in range(3*l*3*l)]
+    if m[:l]==m[l*3*l-3*l:][:l]==q[:1]*l!=m[-l:]==q[8:]*l!=m[~l]==m[l]:
      for k in range(3*l*3*l):g[i+k//(3*l)][j+k%(3*l)]=q[k//(3*l)//l*3+k%(3*l)//l]
  return g
