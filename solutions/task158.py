fb775f00b04a28fe266c21cfdd54d939dedad310
4atj <fourajbira16@gmail.com>
2025-10-08 07:01:44 +0100

    158 -2b

--- solutions/task158.py
+++ solutions/task158.py
@@ -1,15 +1,15 @@
-def p(e):
- for u in range(12):
-  r=3-u//4;e=[[*u] for u in zip(*e[::-1])]
-  for n in range(len(e)+1-3*r):
-   for g in range(len(e[0])+1-3*r):
-    u=[e[n+u//(3*r)][g+u%(3*r)] for u in range(3*r*3*r)]
-    if u[0]!=u[8]!=3<len({*u}):d=u
- for u in range(12):
-  r=3-u//4;e=[[*u] for u in zip(*e[::-1])]
-  for n in range(len(e)+1-3*r):
-   for g in range(len(e[0])+1-3*r):
-    u=[e[n+u//(3*r)][g+u%(3*r)] for u in range(3*r*3*r)]
-    if u[:r]==u[r*3*r-3*r:][:r]==d[:1]*r!=u[-r:]==d[8:]*r!=u[~r]==u[r]:
-     for u in range(3*r*3*r):e[n+u//(3*r)][g+u%(3*r)]=d[u//(3*r)//r*3+u%(3*r)//r]
- return e
+def p(n):
+ for h in range(12):
+  d=3-h//4;n=[[*h]for h in zip(*n[::-1])]
+  for e in range(len(n)+1-3*d):
+   for g in range(len(n[0])+1-3*d):
+    i=[n[e+h//(3*d)][g+h%(3*d)]for h in range(3*d*3*d)]
+    if i[0]!=i[8]!=3<len({*i}):p=i
+ for h in range(12):
+  d=3-h//4;n=[[*h]for h in zip(*n[::-1])]
+  for e in range(len(n)+1-3*d):
+   for g in range(len(n[0])+1-3*d):
+    i=[n[e+h//(3*d)][g+h%(3*d)]for h in range(3*d*3*d)]
+    if i[:d]==i[d*3*d-3*d:][:d]==p[:1]*d!=i[-d:]==p[8:]*d!=i[~d]==i[d]:
+     for h in range(3*d*3*d):n[e+h//(3*d)][g+h%(3*d)]=p[h//(3*d)//d*3+h%(3*d)//d]
+ return n
