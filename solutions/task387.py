6b47f6faf9956e126f5d3ae857f684e2693df2d9
Lucas Deutschmann <lucas.deutschmann@web.de>
2025-10-09 22:52:05 +0200

    387 -11b (post compression)

--- solutions/task387.py
+++ solutions/task387.py
@@ -1,10 +1,10 @@
 def p(o):
  for q in range(4):
-  r=sum(o,[]);n,q,p,*n={r.index(q):0for q in r}
-  while p-q>3:q+=2;p-=2;r[p]=r[q]=5
+  r=sum(o,[]);n,v,p,*n={r.index(q):0for q in r}
+  for q in range(p-v>>2):r[p+~q*2]=r[v-~q*2]=5
   o=[r[q-len(o[0])::-len(o[0])]for q in range(len(o[0]))]
  for q in range(4):
-  r=sum(o,[]);n,q,p,*n={r.index(q):0for q in r}
-  for q in[1,3,1+len(o[0]),2+len(o[0]),3+len(o[0]),1-len(o[0]),2-len(o[0]),3-len(o[0])]:r[p-q]=r[n[0]]
+  r=sum(o,[]);n,v,p,*n={r.index(q):0for q in r}
+  for q in range(8):r[p-q%-3+~(~-q//3%-3)*len(o[0])-3]=r[n[0]]
   o=[r[q-len(o[0])::-len(o[0])]for q in range(len(o[0]))]
  return o
