1ee583a31c4743f6ac7c26e65001dd746b3453d6
4atj <fourajbira16@gmail.com>
2025-10-09 04:00:51 +0100

    096 -10b

--- solutions/task096.py
+++ solutions/task096.py
@@ -1,6 +1,6 @@
 import re
-def p(l):
- f=re.sub(', ','',str(l+[*zip(*l)]));i=int(max(f,key=f.count));a={0:(0,i)}
- for m in range(10):
-  if(m!=i)*re.findall(f'{m}+',f+f[::-1]):n=len(max(re.findall(f'{m}{m}([^]){m}]+){m}|$',f+f[::-1])));a[len(max(re.findall(f'{m}+',f+f[::-1])))*((n>0)+1)+n>>1]=1+n>>1,m
- return[[[a[max(abs(m),abs(e))][1],i][a[max(abs(m),abs(e))][0]>min(abs(m),abs(e))]for m in range(-max(a),max(a)+1)]for e in range(-max(a),max(a)+1)]
+def p(a):
+ s=re.sub(', ','',str(a+[*zip(*a)]));f=int(max(s,key=s.count));r={0:(0,f)}
+ for l in range(10):
+  if(l!=f)*re.findall(f'{l}',s):e=len(max(re.findall(f'{l}{l}([^]){l}]+){l}|',s+s[::-1])));r[len(max(re.findall(f'{l}+',s)))*((e>0)+1)+e>>1]=1+e>>1,l
+ return[[[r[max(abs(l),abs(s))][1],f][r[max(abs(l),abs(s))][0]>min(abs(l),abs(s))]for l in range(-max(r),max(r)+1)]for s in range(-max(r),max(r)+1)]
