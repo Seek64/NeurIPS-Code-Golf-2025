fe86dc9915e6e0fd3feae5269958041fa6816476
lukegustafson <lukeg3@gmail.com>
2025-10-09 00:21:17 -0400

    208 -8b

--- solutions/task208.py
+++ solutions/task208.py
@@ -1,2 +1,2 @@
 from re import*
-p=lambda g:eval((i:=min(k:=str(g)+'#[]'*X,key=k.count)).join(split(sub(i,w:=f'[^{i}]',sub(f'({w}+)',r'(\1)',sub(f".(?<={w*S})|(?={w*S}).",".",*findall(i+'.*'+i,k)))),k)))
+p=lambda g:eval((i:=min(k:=str(g)+'#[]'*X,key=k.count)).join(split(sub(i,w:=f'[^{i}]',sub(f'({w}+)',r'(\1)',sub(f".(?<={w*S})|(?={w*S}).",".",k).strip("."))),k)))
