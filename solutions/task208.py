3d416abaff1f0c60440902538dad02926fa2750f
Mukundan314 <mukundan314@gmail.com>
2025-10-10 13:06:55 +0530

    208 -2b

--- solutions/task208.py
+++ solutions/task208.py
@@ -1,2 +1,2 @@
 from re import*
-p=lambda g:eval((i:=min(k:=str(g)+'#[]'*X,key=k.count)).join(split(sub(i,f'){(w:=f"[^{i}]")}(',sub(f".(?<={w*S})|(?={w*S}).",".",k)).strip(".()"),k)))
+p=lambda g:eval((i:=min(k:=str(g)+'#[]'*X,key=k.count)).join(split(sub(i,f')[^{i}](',sub("[^%s]{30,}"%i,lambda x:'.'*len(x[0]),k)).strip(".()"),k)))
