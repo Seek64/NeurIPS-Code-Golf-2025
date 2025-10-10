05ccb93486ecf2fbaacb35cfdc4eb3664443abf6
Mukundan314 <mukundan314@gmail.com>
2025-10-10 12:34:53 +0530

    208 -4b

--- solutions/task208.py
+++ solutions/task208.py
@@ -1,2 +1,2 @@
 from re import*
-p=lambda g:eval((i:=min(k:=str(g)+'#[]'*X,key=k.count)).join(split(sub(i,f'){(w:=f"[^{i}]")}(',sub(f".(?<={w*S})|(?={w*S}).",".",k).strip("."))[1:-1],k)))
+p=lambda g:eval((i:=min(k:=str(g)+'#[]'*X,key=k.count)).join(split(sub(i,f'){(w:=f"[^{i}]")}(',sub(f".(?<={w*S})|(?={w*S}).",".",k)).strip(".()"),k)))
