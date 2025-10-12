990904597b9e931cb85b7649e85314f8f4b83e27
lukegustafson <lukeg3@gmail.com>
2025-10-12 01:57:16 -0400

    255 -8b

--- solutions/task255.py
+++ solutions/task255.py
@@ -1,9 +1,10 @@
 def p(b,n=59):
- a=n//4;e=z=-2
- while z<31-a:
-  z+=1;c=0;d=[[*b]for b in b];l=lambda n,l=9,d=z,a=a:c<31*all(l>>b&1for b in b[c:c+n]for b in b[max(0,d):d+a])
-  if z<0:b=d
+ a=n//4;z=-2
+ for l in b:
+  z+=1;c=0;d=[[*b]for b in b];l=lambda n,l=9,a=a:c<31*all(l>>b&1for b in b[c:c+n]for b in b[z-30:z+a])
   if l(9):
-   while(c:=c+1)*(z>e)*(l(1,1)+l(5)):b[c-1][z+1:z+a-1]=-2%a*[3]
-   if l(5,1,e:=z+1,a-2):b=d
- return b*(3-a)or p([*zip(*b)][::-1],n-1)
+   while(c:=c+1)*(l(1,1)+l(5)):b[c-1][1+z:a-1+z]=(a-2)*[3]
+   z+=1
+   if l(5,1,a-2):b=d
+   z+=4
+ return b*(3-a)or p([[*b]for b in zip(*b)][::-1],n-1)
