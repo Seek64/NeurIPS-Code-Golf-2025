473a1ebfd4cc6d7f8eae5f0aa3609ead9f43bb8c
lukegustafson <lukeg3@gmail.com>
2025-10-12 02:32:42 -0400

    185 -18b

--- solutions/task185.py
+++ solutions/task185.py
@@ -1,1 +1,1 @@
-p=lambda g:[g:=[[x*(P==(P:=x)!=max(Z))for c,x in zip(g,r)if{*c}-Z][1:]for r in zip(*g)if{*r,P:=0}-Z]for Z in({*g[0]},{p})][1]
+p=lambda g,*P:[g:=[[x*(P==(P:=x)!=max(Z))for c,x in zip(g,r)if{*c}-{*Z}][1:]for r in zip(*g)]for Z in g][1]
