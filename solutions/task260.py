5798ed740ac4531e66da68fc5ddc84cfe1dda14a
4atj <fourajbira16@gmail.com>
2025-10-14 12:06:49 +0100

    260 another -1b

--- solutions/task260.py
+++ solutions/task260.py
@@ -1,2 +1,2 @@
-R=range(10)
-p=lambda g:[[g[y][x]**5%415%10or(sum((g[Y]+g*2)[x+i-y-3+Y]==5for i in b""for Y in R)==1)*sum({*max(g)}-{5})for x in R]for y in R]
+R=range
+p=lambda g:[[g[y][x]**5%415%10or(sum((g[i//5]+g*2)[x-y+i%5-2+i//5]==5for i in R(50))==1)*sum({*max(g)}-{5})for x in R(10)]for y in R(10)]
