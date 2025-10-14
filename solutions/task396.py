3a060bd91d5abcf2ce621be67887655b41adf17f
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-14 16:46:02 +1100

    396 -1b

--- solutions/task396.py
+++ solutions/task396.py
@@ -1,2 +1,2 @@
-import re
-def p(g,S=str):*_,a,b=sorted('123456789',key=S(g).count);exec("g[:]=zip(*g[(max(re.findall(b+f'(?:, {b})+',S(g)))in S(g[-1]))-2::-1]);"*96);return eval(re.sub(b,a,S(g)))
+from re import*
+def p(g,S=str):*_,a,b=sorted('123456789',key=S(g).count);exec("g[:]=zip(*g[(max(findall(b+f'(?:, {b})+',S(g)))in S(g[-1]))-2::-1]);"*A);return eval(sub(b,a,S(g)))
