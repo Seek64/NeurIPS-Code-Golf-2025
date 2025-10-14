aca4f5aad38631ff47a2da865d17a3e46f5427b7
4atj <fourajbira16@gmail.com>
2025-10-14 11:42:36 +0100

    260 another -1b

--- solutions/task260.py
+++ solutions/task260.py
@@ -1,2 +1,2 @@
-E=enumerate
-p=lambda g:[[e*(e!=5)or(sum((R+g*2)[x+i-y-3+X]==5for i in b""for X,R in E(g))==1)*sum({*max(g)}-{5})for x,e in E(r)]for y,r in E(g)]
+R=range(10)
+p=lambda g:[[(o:=g[y][x])*(o!=5)or(sum((g[Y]+g*2)[x+i-y-3+Y]==5for i in b""for Y in R)==1)*sum({*max(g)}-{5})for x in R]for y in R]
