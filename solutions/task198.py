b4e2550f6254ea622a274973f3f8759dbb86a110
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-14 16:42:54 +1100

    198 -5b

--- solutions/task198.py
+++ solutions/task198.py
@@ -1,2 +1,1 @@
-import re
-p=lambda g,n=-59,a='3|0':n*g or p(eval(re.sub(a,a[0],f"{*zip(*g[::-1]),}")),n+1,r'4|3(?=, 4|[^)]*([^34]), \1)')
+import re;p=lambda g,n=23:-n*g or eval(re.sub(r'0|3(?=, 4|[^)]*([^34]), \1)','43'[n<1],f"{*zip(*p(g,n-1)[::-1]),}"))
