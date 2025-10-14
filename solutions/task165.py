56d6f47a9fc47ec4b02f045cb2906144a239828d
Mukundan314 <mukundan314@gmail.com>
2025-10-14 14:23:31 +0530

    165 -1b

--- solutions/task165.py
+++ solutions/task165.py
@@ -1,1 +1,1 @@
-p=lambda g:[*zip(*[r[:-(k:=r[::-1].index(max(r,key=sum(g[::-1],g).index)))]+k*[max(r[-k:])]+r for*r,in zip(*g)])]
+p=lambda g:[*zip(*map(lambda*r:r[:-(k:=r[::-1].index(max(r,key=sum(g[::-1],g).index)))]+k*(max(r[-k:]),)+r,*g))]
