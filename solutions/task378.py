25c7aaf7a25d722d2d7bd37f6ab8ee2b9996bcab
4atj <fourajbira16@gmail.com>
2025-10-15 03:34:05 +0100

    378 -1b

--- solutions/task378.py
+++ solutions/task378.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub(r"(?=(.{%d})+([^0]), \2.{%d}\2, 0, (.))0"%(x:=len(g)*3+5,x*2-10),r"\3",f"{*zip(*g[::-1]),}"))for _ in g][3]
+p=lambda g:[g:=eval(re.sub(r"(?=(.{%d}.{5})+([^0]), \2.{%d}\2, 0, (.))0"%(x:=len(g)*3,x*2),r"\3",f"{*zip(*g[::-1]),}"))for _ in g][3]
