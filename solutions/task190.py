fce9bc35272683fa7b9b9b9e28ed1f6195bc6670
Mukundan314 <mukundan314@gmail.com>
2025-10-16 18:05:29 +0530

    190 -2b

--- solutions/task190.py
+++ solutions/task190.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g,k=19:-k*g or p(eval(re.sub("0((.{31}0, ([^0])){2})",r"\3\1",f"{*zip(*g[::-1]),}")),k-1)
+p=lambda g:g[200:]or p(eval(re.sub("0((.{31}0, ([^0])){2})",r"\3\1",f"{*g,*zip(*g[:-11:-1])}")))
