5da33220eede401517dc66363853d44b66f6b0c2
Mukundan314 <mukundan314@gmail.com>
2025-10-16 13:01:11 +0530

    069 -3b

--- solutions/task069.py
+++ solutions/task069.py
@@ -1,6 +1,6 @@
 R=range(320)
 def p(g):
- *f,=U=b'%r'%g;P=[i for i in R if 88%f[i]%8]
+ *f,=U=b'%r'%g
  for i in R:
-  for j in(f[i]==56)*P:f[j],f[i+j-P[0]]=48,U[j]
+  for j in(P:=[j for j in R if U[j]&503%f[i]>48]):f[j],f[i+j-P[0]]=48,U[j]
  return eval(bytes(f))
