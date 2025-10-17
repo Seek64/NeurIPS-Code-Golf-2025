730796e35407306ef6fecbfbbd7f3a0519cdb789
Lucas Deutschmann <deutschmann@eit.uni-kl.de>
2025-10-17 11:28:39 +0200

    118 -4b (post compression)

--- solutions/task118.py
+++ solutions/task118.py
@@ -3,6 +3,6 @@ def p(l,e=set()):
   return [l and(p(l[1:],e)or not l[0]&e and p(l[1:],e|l[0])),e][d<e]
  d={(u,n)for u in range(len(l))for n in range(len(l[0]))if l[u][n]&2}
  for n in 2,3:
-  if e:=p([e for f in range(len(l))for e in range(len(l[0]))for e in[{(u,n)for n in range(-n,1+n)for u,n in[(f+n,e),(f,e+n)]if len(l)>u>-1<n<len(l[0])}]if min(l[u][n]for u,n in e)]):
+  if e:=p([e for f in range(len(l)-n)for e in range(len(l[0])-n)for e in[{(u,n)for n in range(-n,1+n)for u,n in[(f+n,e),(f,e+n)]if 1+n}]if min(l[u][n]for u,n in e)]):
    for u,n in e:l[u][n]+=l[u][n]-2
    return l
