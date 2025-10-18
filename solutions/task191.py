9f74a7f6d1e0d7e07fc5a66bf30578e91c700dd3
4atj <fourajbira16@gmail.com>
2025-10-18 19:49:59 +0100

    191 -1b (post compression)

--- solutions/task191.py
+++ solutions/task191.py
@@ -1,8 +1,8 @@
 import re
-def p(e):
- def p(e):
-  return'(%s)'%re.sub('[^%a]'%(4-(')'in e[0])),'.',e[0])
- for a in[1,-1,1]*4:*e,=zip([0]*len(e[0]),*e[::a])
- f=re.sub(*'10',re.sub('[^1]+',p,re.search('1.*1','(%s)'%e)[0]))
- for a in[1,-1,1]*4:*e,=zip(*eval('1'.join(re.split(f,'(%s)'%e[::a]))))
- return[a[3:-3]for a in e[3:-3]]
+def p(l):
+ def p(l):
+  return'(%s)'%re.sub('[^%s]'%(4-(')'in l[0])),'.',l[0])
+ for e in[1,-1,1]*4:*l,=zip([0]*len(l[0]),*l[::e])
+ f=re.sub(*'10',re.sub('[^1]+',p,re.search('1.*1','(%s)'%l)[0]))
+ for e in[1,-1,1]*4:*l,=zip(*eval('1'.join(re.split(f,'(%s)'%l[::e]))))
+ return[e[3:-3]for e in l[3:-3]]
