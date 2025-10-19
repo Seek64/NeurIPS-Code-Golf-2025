42a6ff7c60ac3be9d28997521db3e68556a04466
4atj <fourajbira16@gmail.com>
2025-10-19 07:21:24 +0100

    151 -7b

--- solutions/task151.py
+++ solutions/task151.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub("0,(?=.{%d}[^0]) [^0]"%len(3*g),"4,4",f"{*zip(*g[::-1]),}"))for _ in g][3]
+p=lambda g:[g:=eval(re.sub("0, [^0](?=[^(]*\([^0])","4,4",f"{*zip(*g[::-1]),}"))for _ in g][3]
