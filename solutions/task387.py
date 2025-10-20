ad9d6e11fbcb2b42878def0490f50a18bd9b7504
4atj <fourajbira16@gmail.com>
2025-10-20 21:55:16 +0100

    387 -2b

--- solutions/task387.py
+++ solutions/task387.py
@@ -5,6 +5,6 @@ def p(o):
   o=[r[q-len(o[0])::-len(o[0])]for q in range(len(o[0]))]
  for q in range(4):
   r=sum(o,[]);n,v,p,*n={r.index(q):0for q in r}
-  for q in range(8):r[p-q%-3+~(~-q//3%-3)*len(o[0])-3]=r[n[0]]
+  for q in range(8):r[p-q%-3+~(q//3%-3)*len(o[0])-3]=r[n[0]]
   o=[r[q-len(o[0])::-len(o[0])]for q in range(len(o[0]))]
  return o
