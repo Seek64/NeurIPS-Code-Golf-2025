1a1a57ff2c43dd0475c7e3e7647e9ae25a02d168
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-23 16:09:36 +1100

    250 -1b

--- solutions/task250.py
+++ solutions/task250.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:eval(re.sub("5(([^2]{31}0)*)(?=[^2]*\).*2)",r"0\1+5",f"{*zip(*g[327670:]or p(g*2)),}"))[::-1]
+p=lambda g:exec(r'g[::-1]=zip(*eval(re.sub("5(([^2]{31}0)*)(?=[^2]*\).*2)",r"0\1+5",str(g))));'*20)or g
