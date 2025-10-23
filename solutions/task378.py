ed81ad6429994a108730bcb220004fa2e47a8f2a
lukegustafson <lukeg3@gmail.com>
2025-10-23 03:38:48 -0400

    378 -1b

--- solutions/task378.py
+++ solutions/task378.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(r'g[::-1]=zip(*eval(re.sub(r"(?=(.{%d}.{5})+([^0]), \2.{%d}\2, 0, (.))0"%(x:=len(g)*3,x*2),r"\3",str(g))));'*4)or g
+p=lambda g:exec(fr'g[::-1]=zip(*eval(re.sub(r"(?=(.{{{(x:=len(g)*3)+5}}})+([^0]), \2{".."*x}\2, 0, (.))0",r"\3",str(g))));'*4)or g
