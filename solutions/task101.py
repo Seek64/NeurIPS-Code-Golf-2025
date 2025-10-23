8567f519d9ef357ad5b4bc1be1a519d184da530a
4atj <fourajbira16@gmail.com>
2025-10-23 14:34:04 +0100

    101 -6b (post compression)

--- solutions/task101.py
+++ solutions/task101.py
@@ -1,13 +1,13 @@
-def p(m):
- i,*o=len(m[0])+2,
- for f in m:o+=*f,0,0
- d=[o.index(1)]
- for f in d:d+=[f for f in(f-i,f-1,f+1,f+i)if(f in d)==0<f<len(o)>0<o[f]]
- j=min(f for f in d if o[f]>1);n=d
- for r in range(len(o)):
-  d=[r]
-  for f in d:d+=[f for f in(f-i,f-1,f+1,f+i)if(f in d)==0<f<len(o)>0<o[f]]
-  l=6+26%len(d)>>2
-  for e in n:
-   for f in range(l*l*all(o[f]>1for f in d)):o[r+l*e-l*j+f//l*i+f%l]=o[e]
- return[o[r*i:][:i-2]for r in range(len(m))]
+def p(e):
+ m,*t=len(e[0])+2,
+ for f in e:t+=*f,0,0
+ i=[t.index(1)]
+ for f in i:i+=[f for f in(f-m,f-1,f+1,f+m)if len(t)>f>(f in i)*m<t[f]]
+ n=i
+ for r in range(len(t)):
+  i=[r]
+  for f in i:i+=[f for f in(f-m,f-1,f+1,f+m)if len(t)>f>(f in i)*m<t[f]]
+  l=6+26%len(i)>>2
+  for a in n:
+   for f in range(l*l*all(t[f]>1for f in i)):t[r+l*a-l*min(f for f in n if t[f]>1)+f//l*m+f%l]=t[a]
+ return[t[r*m:][:m-2]for r in range(len(e))]
