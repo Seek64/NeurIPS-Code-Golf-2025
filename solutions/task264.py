6859e243bb7f6135beb7992c604cd2bfd7f86329
lukegustafson <lukeg3@gmail.com>
2025-10-23 03:29:21 -0400

    264 -2b

--- solutions/task264.py
+++ solutions/task264.py
@@ -1,1 +1,1 @@
-T=1,2,3;p=lambda g:[sum([eval(f"sorted([0in(S:=sum(g,T)),*[i!=5for i in S],*g]{'for*g,in map(zip,g,g[1:],g[2:])'*2})#{g}")[(X+77^57*-Y)%9][x-4]for Y in T],())for X in T for x in T]
+T=3,2,1;p=lambda g:[sum([eval(f"sorted([0in(S:=sum(g,T)),*[i!=5for i in S],*g]{'for*g,in map(zip,g,g[1:],g[2:])'*2})#{g}")[42>>Y&7*~-X^Y][-x]for Y in T],())for X in T for x in T]
