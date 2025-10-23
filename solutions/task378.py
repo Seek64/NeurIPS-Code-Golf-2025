248574ee30d5c5008959129485f0b6e1b3398ccb
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-23 13:33:59 +1100

    378 -2b

--- solutions/task378.py
+++ solutions/task378.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:[g:=eval(re.sub(r"(?=(.{%d}.{5})+([^0]), \2.{%d}\2, 0, (.))0"%(x:=len(g)*3,x*2),r"\3",f"{*zip(*g[::-1]),}"))for _ in g][3]
+p=lambda g:exec(r'g[::-1]=zip(*eval(re.sub(r"(?=(.{%d}.{5})+([^0]), \2.{%d}\2, 0, (.))0"%(x:=len(g)*3,x*2),r"\3",str(g))));'*4)or g
