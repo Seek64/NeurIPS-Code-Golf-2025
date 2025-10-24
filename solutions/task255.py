69d22a195d6d6210ce75646872d469944f60af6d
4atj <fourajbira16@gmail.com>
2025-10-24 20:12:16 +0100

    255 -1b (post compression)

--- solutions/task255.py
+++ solutions/task255.py
@@ -1,10 +1,10 @@
-def p(e,h=51,i=-2):
- f=h//4
- for l in e:
+def p(b,e=51,i=-2):
+ f=e//4
+ for h in b:
   i+=1
-  o=[[*o]for o in e];l=0;g=lambda o,k=9,g=f:l<31*all(k>>o&1for o in e[l:l+o]for o in o[i-30:i+g])
+  d=[[*d]for d in b];h=0;g=lambda d,f=9,g=f:h<31*all(f>>d&1for d in b[h:h+d]for d in d[i-30:i+g])
   if g(9):
-   while(l:=l+1)*(g(1,1)+g(5)):e[l-1][1+i:f-1+i]=(f-2)*[3]
-   if g(5,1,f-2):e=o
+   while(h:=h+1)*(g(1,1)+g(5)):b[h-1][1+i:f-1+i]=(f-2)*[3]
+   if g(5,1,f-2):b=d
    i+=5
- return e*(3-f)or p([[*o]for o in zip(*e)][::-1],h-1)
+ return b*(3-f)or p([[*d]for d in zip(*b)][::-1],e-1)
