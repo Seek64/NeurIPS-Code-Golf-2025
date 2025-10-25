b03247ae650dda3dc4e854e1467326d74ddf8b15
4atj <fourajbira16@gmail.com>
2025-10-25 14:32:15 +0100

    367 -1b

--- solutions/task367.py
+++ solutions/task367.py
@@ -1,1 +1,1 @@
-p=lambda g,k=95:-k*g or p([g:=[c*((r!=[5]*3)>c%-4<r[2])|(96-k in l)*4for*r,c in zip([4]*2+g,g,[4]+l,l)]for*l,in zip(*g[::k%3-1|1])],k-1)
+p=lambda g,k=94:~k*g or p([g:=[c*((r!=[5]*3)>c%-4<r[2])|(k^95in l)*4for*r,c in zip([4]*2+g,g,[4]+l,l)]for*l,in zip(*g[::k%3-1|1])],k-1)
