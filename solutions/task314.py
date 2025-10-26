5caa59de4b93af008a330e5a40761cddf3249906
Mukundan314 <mukundan314@gmail.com>
2025-10-26 16:19:25 +0530

    314 -12b

--- solutions/task314.py
+++ solutions/task314.py
@@ -1,2 +1,1 @@
-import re
-p=lambda g:eval(re.sub(f"1(?={'.{%d}([2-9])'*2})|"*2%(8,190,77,52),r"\1\3",f"{g}#"*2))
+p=lambda g,*r:[*map(max,r,(g[:2]==g[6:])*3*g[:3]+g)]or[*map(p,g,*map(p,zip(*g),*g))]
