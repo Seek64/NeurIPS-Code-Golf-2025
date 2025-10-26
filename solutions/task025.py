bfaea67b37bfb19e61a30b8e6eecdaefce5951b0
lukegustafson <lukeg3@gmail.com>
2025-10-26 04:23:26 -0400

    025 -4b

--- solutions/task025.py
+++ solutions/task025.py
@@ -1,1 +1,1 @@
-p=lambda g,N=0:[g:=[[N+(N:=all(c)*(I:=(x:=r.pop())in r)*x)|x*(I|all(c)|any(min(g)))for c in g[::-1]]for*r,in zip(*g)]for _ in g][3]
+p=lambda g,N=0:[g:=[[any(min(g)+[I:=(x:=r.pop())in r,A:=all(c)])*x|N+(N:=A*I*x)for c in g[::-1]]for*r,in zip(*g)]for _ in g][3]
