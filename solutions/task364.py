32647b0daab0767412c692e5cb849ace225cb574
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-27 10:05:42 +1100

    364 -1b

--- solutions/task364.py
+++ solutions/task364.py
@@ -1,1 +1,1 @@
-p=lambda g,k=95:-k*g or p([g:=[c and[c|a|all([a,b*-1,k>87])<<k,c%3*4%5^2][k<2]for c,a,b in zip(r,[0]+r,g)]for*r,in zip(*g[::-1])],k-2)
+p=lambda g,k=95:-k*g or p([g:=[[7&88>>c%7,c|a|all([a,b*-1,k>83])<<k*3][k>0<c]for c,a,b in zip(r,[0]+r,g)]for*r,in zip(*g[::-1])],k-1)
