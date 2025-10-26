deb2d21934837cde0563c5423cc01d25bc748583
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-27 09:40:26 +1100

    371 -12b

--- solutions/task371.py
+++ solutions/task371.py
@@ -1,2 +1,1 @@
-import re
-p=lambda g,n=3:g*-n or[*zip(*eval(re.sub(r'(1(.*)).{6}(0\2(1))|, 31,',r'\1+3,3\4,3+\3',str(p(g,n-1)))))]
+p=lambda g,u=0:eval((G:=f"{*zip(*u or p(g,g)),}")[:(w:=G.rfind("1")+G.find("1")>>1)-3]+"3,"*3+G[w+5:])
