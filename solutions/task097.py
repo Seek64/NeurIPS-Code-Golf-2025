615bedc1a4f810bc07faa1839cfa515fc490ac2c
lukegustafson <lukeg3@gmail.com>
2025-10-27 17:52:07 -0400

    097 -12b

--- solutions/task097.py
+++ solutions/task097.py
@@ -1,2 +1,1 @@
-E=enumerate
-p=lambda g:[[x*(sum(sum([0,*s][j:j+3])for s in[[],*g][i:i+3])>x)for j,x in E(r)]for i,r in E(g)]
+exec("p=lambda g,Z=[[0]*30]:[[g[1][1]&sorted(b'%r'%g)[-4]"+"for*g,in map(zip,Z+g,g,g[1:]+Z)]"*2)
