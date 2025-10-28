51ee21af8ee9f1fcace3c544c613221f4462d287
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-28 18:31:15 +1100

    -11b (ft Mukundan314)

--- solutions/task191.py
+++ solutions/task191.py
@@ -1,5 +1,5 @@
 from re import*
 def p(l):
- K=[[0]*U];f=sub('1',')0(',sub('[^1]+'*S,lambda l:'.'*len(l[0]),str(l:=K+[[0,*r,0]for r in l]+K))).strip('(.)')
- for e in[1,-1,1]*8:l[::e]=zip(*eval('1'.join(split(f,'%s'%l))))
- return[e[1:-1]for e in l[1:-1]]
+ X=[0]*U;r=*l,=X,*zip(X,*l,X),X
+ for e in[1,-1,1]*12:l[::e]=zip(*eval('1'.join(split(sub('1',')0(',sub('[^1]+'*S,lambda l:'.'*len(l[0]),str(r))).strip('(.)'),'%s'%l))))
+ return[*zip(*l[1:-1])][1:-1]
