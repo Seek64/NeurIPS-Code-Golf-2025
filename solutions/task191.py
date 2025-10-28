29acaf1772005aa67cbe7b4963702d85625ea970
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-28 18:10:33 +1100

    191 -7b

--- solutions/task191.py
+++ solutions/task191.py
@@ -1,8 +1,5 @@
-import re
+from re import*
 def p(l):
- def p(l):
-  return'(%s)'%re.sub('[^%s]'%(4-(')'in l[0])),'.',l[0])
- for e in[1,-1,1]*4:*l,=zip([0]*len(l[0]),*l[::e])
- f=re.sub(*'10',re.sub('[^1]+',p,re.search('1.*1','(%s)'%l)[0]))
- for e in[1,-1,1]*4:*l,=zip(*eval('1'.join(re.split(f,'(%s)'%l[::e]))))
- return[e[3:-3]for e in l[3:-3]]
+ K=[[0]*U];f=sub('1',')0(',sub('[^1]+'*S,lambda l:'.'*len(l[0]),str(l:=K+[[0,*r,0]for r in l]+K))).strip('(.)')
+ for e in[1,-1,1]*8:l[::e]=zip(*eval('1'.join(split(f,'%s'%l))))
+ return[e[1:-1]for e in l[1:-1]]
