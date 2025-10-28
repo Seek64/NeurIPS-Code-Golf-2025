38599a5117e8835124f8217e90e05fa163f2ff50
lukegustafson <lukeg3@gmail.com>
2025-10-28 01:26:06 -0400

    141 -1b

--- solutions/task141.py
+++ solutions/task141.py
@@ -1,1 +1,1 @@
-p=lambda g:[[*map(max,r[:(a:=abs(g.index(m:=max(g))-i))]+m,m[a:]+r,r)]for i,r in enumerate(g)]
+p=lambda g,i=-1:[[*map(max,r[:(a:=abs(g.index(m:=max(g))-(i:=i+1)))]+m,m[a:]+r,r)]for r in g]
