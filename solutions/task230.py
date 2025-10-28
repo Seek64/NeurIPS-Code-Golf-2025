6452da07fb7758d0eb9280e36c7db393a8b07422
lukegustafson <lukeg3@gmail.com>
2025-10-28 13:11:38 -0400

    230 -9b

--- solutions/task230.py
+++ solutions/task230.py
@@ -1,2 +1,1 @@
-import re
-p=lambda g:[g:=eval(re.sub('0(?=(.{%d}5){2})'%(len(g)*3+4),k,f"{*zip(*g[::-1]),}"))for k in'3421'][3]
+exec("p=lambda g,Z=[[0]*30]:[[(114>>hash((*b'%rr&R'%g,))%9^2)%6"+"for*g,in map(zip,Z+g,g,g[1:]+Z)]"*2)
