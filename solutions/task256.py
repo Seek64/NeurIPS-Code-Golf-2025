19d66567ff95281ed033e771299d39db72623dbf
lukegustafson <lukeg3@gmail.com>
2025-10-29 03:11:48 -0400

    256 -1b

--- solutions/task256.py
+++ solutions/task256.py
@@ -1,4 +1,4 @@
 def p(g):
-	w=sum(m:=max(g))//2;i=w+g.index(m)
-	for r in g:r[:i]=[(i>w)-~(i>=w)]*i;i-=i>0
-	return g
+ w=sum(m:=max(g))//2;i=w+g.index(m)
+ for r in g:r[:i]=[2+(i-w|8)//9]*i;i-=i>0
+ return g
