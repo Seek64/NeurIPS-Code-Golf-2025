4462e603da720a6890455591fcd36c859192d342
lukegustafson <lukeg3@gmail.com>
2025-10-29 01:56:01 -0400

    253 -8b

--- solutions/task253.py
+++ solutions/task253.py
@@ -1,2 +1,1 @@
-F=0,1,2,3
-p=lambda g:[[max(min(g[p//12+v//2][p%12+v%2]for v in F if~y&2^(2>x)^v|x*y%3)for p in range(144))for x in F]for y in F]
+F=0,1,2,3;p=lambda g:[[(x*y%3<1)*max(map(min,S:=sum(g[::-1],[]),S[298%(9|x^y+6):],S[13+(2>x^y):]))for x in F]for y in F]
