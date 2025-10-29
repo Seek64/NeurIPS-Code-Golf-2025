fb2553f54867f3782a7ea7093fb6cb160e22f6e7
Mukundan314 <mukundan314@gmail.com>
2025-10-29 17:56:53 +0530

    125 -1b

--- solutions/task125.py
+++ solutions/task125.py
@@ -1,2 +1,2 @@
 import re
-p=lambda g:exec(r'g[::-1]=zip(*eval(re.sub("[38](?=..[46].{40}[46])|(8)(?=.{49}6)",r"4\1%5",str(g))));'*20)or g
+p=lambda g:exec(r'g[::-1]=zip(*eval(re.sub("[38](?=..[46].{40}[46]|.{49}(4|6))",r"4-1%1\1",str(g))));'*20)or g
