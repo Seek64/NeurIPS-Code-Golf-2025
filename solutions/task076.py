730489c2f38c97f65f34408c823196683ff446ef
hashkitten <10756469+hashkitten@users.noreply.github.com>
2025-10-30 22:22:20 +1100

    076 -23b

--- solutions/task076.py
+++ solutions/task076.py
@@ -1,7 +1,13 @@
-def p(r):
- d=[[*b,0,0][:15]for b in r]+[[0]*15]*2;p=b'%r#'%d*2;i={p.find(49)}
- for b in range(8484):
-  *d,=zip(*d[:15][::1|1-b%3]);*l,=n=b'%r#'%d*2;i={e+b-51for e in i for b in b"06_be"if p[e+b-51]%49<6}
-  for e in i:l[e+b%707-min(i)]=p[e]
-  if all(n in{49,51,d}for n,d in zip(l,n)):d=eval(bytes(l))
- return[d[:len(n)]for n,d in zip(r,d)]
+def p(g):
+ *G,=g
+ for y in range(len(g)):
+  for x in range(len(g[0])):
+   if G[y][x]==1:s={(x,y)}
+ for y in g:s={(X+x,Y+y)for x,y in s for X in range(-1,2)for Y in range(-1,2)if Y+y in range(len(g))!=X+x in range(len(g[0]))!=0<g[Y+y][X+x]}
+ for y in(1,1,-1)*4:
+  g=[x for*x,in zip(*g[::y])]
+  for Y in range(-13,13):
+   for X in range(-13,13):
+    if all(Y+y in range(len(g))!=X+x in range(len(g[0]))!=G[y][x]in(1,3,g[Y+y][X+x])for x,y in s):
+     for x,y in s:g[Y+y][X+x]=G[y][x]
+ return g
