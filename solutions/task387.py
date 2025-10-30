beb802882deeb8a81040819dc7146b3913a61b6e
lukegustafson <lukeg3@gmail.com>
2025-10-30 12:46:32 -0400

    387 -27b

--- solutions/task387.py
+++ solutions/task387.py
@@ -1,10 +1,1 @@
-def p(f):
- for i in range(4):
-  d=sum(f,[]);b,h,g,*e={d.index(i):0for i in d}
-  for i in range(g-h>>2):d[g+~i*2]=d[h-~i*2]=5
-  f=[d[i-len(f[0])::-len(f[0])]for i in range(len(f[0]))]
- for i in range(4):
-  d=sum(f,[]);b,h,g,*e={d.index(i):0for i in d}
-  for i in range(8):d[g-i%-3+~(i//3%-3)*len(f[0])-3]=d[e[0]]
-  f=[d[i-len(f[0])::-len(f[0])]for i in range(len(f[0]))]
- return f
+p=lambda g,n=11:-n*g or[[[(i:=((x:=r.pop())+i>0)*-~i)%2*sum(r[:2-i])**4%5*5or x,x or P%5and P|80,sum({*sum(g*(x>9),[])}-{5,x%16})or x][n//4]for P in[0]+r[:0:-1]]for*r,in zip(*p(g,n-1))if[i:=0]]
