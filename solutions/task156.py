d13d5524fa516a66d3de757a642f02e522350228
Mukundan314 <mukundan314@gmail.com>
2025-10-30 07:16:50 +0530

    156 -2b

--- solutions/task156.py
+++ solutions/task156.py
@@ -1,2 +1,2 @@
 import re
-def p(g):g,n=re.subn("(?<=4.{34})4(?=.{34}4(.*0.{31}(4))?)",r"n*3//g.count('4%')*1\2%3",str(g));return eval(g)
+p=lambda g:eval(g:=re.sub("(?<=4.{34}4)(?=.{34}4(.*0.{31}(4))?)",r"*(X:=g.count)('X\2X')//X('+')+1",str(g)))
