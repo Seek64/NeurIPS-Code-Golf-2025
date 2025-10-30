3e24f7d14ec64303e268e508a55e4662b10c258c
4atj <fourajbira16@gmail.com>
2025-10-30 18:15:34 +0100

    076 -2b (post compression)

--- solutions/task076.py
+++ solutions/task076.py
@@ -1,13 +1,13 @@
 def p(r):
  a=r
- for l in range(len(r)):
-  for e in range(len(r[0])):
-   if a[l][e]==1:q={(e,l)}
- for l in r:q={(e+z,i+l)for z,l in q for e in range(-1,2)for i in range(-1,2)if i+l in range(len(r))!=e+z in range(len(r[0]))!=0<r[i+l][e+z]}
- for l in(1,1,-1)*4:
-  r=[z for*z,in zip(*r[::l])]
-  for i in range(-13,13):
-   for e in range(-13,13):
-    if all(i+l in range(len(r))!=e+z in range(len(r[0]))!=a[l][z]in(1,3,r[i+l][e+z])for z,l in q):
-     for z,l in q:r[i+l][e+z]=a[l][z]
+ for n in range(len(r)):
+  for f in range(len(r[0])):
+   if a[n][f]==1:z={(f,n)}
+ for n in r:z={(f+l,e+n)for l,n in z for f in range(-1,2)for e in range(-1,2)if e+n in range(len(r))!=f+l in range(len(r[0]))!=0<r[e+n][f+l]}
+ for n in(1,1,-1)*4:
+  r=[z for*z,in zip(*r[::n])]
+  for e in range(-13,13):
+   for f in range(-13,13):
+    if all(e+n in range(len(r))!=f+l in range(len(r[0]))!=a[n][l]in(1,3,r[e+n][f+l])for l,n in z):
+     for l,n in z:r[e+n][f+l]=a[n][l]
  return r
