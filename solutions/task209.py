e7d7bd8848ed7d76e91c4968de503e944a86f057
4atj <fourajbira16@gmail.com>
2025-10-30 05:00:24 +0100

    209 -1b

--- solutions/task209.py
+++ solutions/task209.py
@@ -1,7 +1,7 @@
-def p(i):
- e=[(n^4,d,x)for d,i in enumerate(i)for x,n in enumerate(i)if n]
- for x,n in enumerate(i):
-  for d,t,n in e[e.index(sorted(e)[3])+1:]:
-   f=[n*9for n in i*2];r=5
-   for d,l,o in e[e.index(sorted(e)[3])+1:]:l=e[2][1]+x*(l-t);r+=x*x*(f[l][e[2][2]+x*(o-n)]==d^4)+1;exec("f[l][e[2][2]+x*(o-n):e[2][2]+x*(o-n)+x]=[d^4]*x;l+=1;"*x)
-   if r>len(e):return[n[sorted(e)[0][2]:sorted(e)[3][2]+1]for n in f[sorted(e)[0][1]:sorted(e)[3][1]+1]]
+def p(g):
+ e=[(n^4,r,o)for r,g in enumerate(g)for o,n in enumerate(g)if n]
+ for o,n in enumerate(g):
+  for q,j,n in e[e.index(sorted(e)[3])+1:]:
+   d=[r*9for r in g*2];f=5
+   for q,r,l in e[e.index(sorted(e)[3])+1:]:r=e[2][1]+o*(r-j);f+=o*o*(d[r][e[2][2]+o*(l-n)]==q^4)+1;exec(o*"d[r][e[2][2]+o*(l-n):e[2][2]+o*(l-n)+o]=o*[q^4];r+=1;")
+   if f>len(e):return[r[e[0][2]:sorted(e)[3][2]+1]for r in d[e[0][1]:sorted(e)[3][1]+1]]
